879da220ca385ff43df6bd2038f9c0fd30793aac
Patrick Obermueller <pat.obermueller@gmail.com>
2023-05-01 13:04:36 +0200

    feature: add to and from date search
    Text '2023-05-10T00:00:00Z' could not be parsed, unparsed text found at index 10

--- publisher-html/src/main/kotlin/events/boudicca/publisherhtml/restcontroller/SearchRestController.kt
+++ publisher-html/src/main/kotlin/events/boudicca/publisherhtml/restcontroller/SearchRestController.kt
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
 
 @RestController
 @RequestMapping("/api")
@@ -14,8 +16,17 @@ class SearchRestController @Autowired constructor(private val eventService: Even
 
     @GetMapping("/search")
     @ResponseBody
-    fun search(@RequestParam("name") name: String): ResponseEntity<Set<Event>> {
-        val events = eventService.search(SearchDTO().name(name))
+    fun search(@RequestParam("name") name: String,
+               @RequestParam("fromDate") fromDate: String,
+               @RequestParam("toDate") toDate: String): ResponseEntity<Set<Event>> {
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val fromDateParsed = OffsetDateTime.parse(fromDate + "T00:00:00Z", formatter)
+        val toDateParsed = OffsetDateTime.parse(toDate + "T00:00:00Z", formatter)
+
+        val events = eventService.search(SearchDTO()
+                .name(name)
+                .fromDate(fromDateParsed)
+                .toDate(toDateParsed))
         return ResponseEntity(events, HttpStatus.OK)
     }
 }

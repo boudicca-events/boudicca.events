519b3eff1b43c66e1d99c9498464d343b8b1fcc6
Patrick Obermueller <pat.obermueller@gmail.com>
2023-05-07 19:59:58 +0200

    tech: prepare data structure for html client

--- publisher-html/src/main/kotlin/events/boudicca/publisherhtml/service/EventService.kt
+++ publisher-html/src/main/kotlin/events/boudicca/publisherhtml/service/EventService.kt
@@ -1,15 +1,18 @@
 package events.boudicca.publisherhtml.service
 
-import events.boudicca.api.Event
-import events.boudicca.api.util.EventMapper
+import events.boudicca.SemanticKeys
 import events.boudicca.openapi.ApiClient
 import events.boudicca.openapi.api.EventPublisherResourceApi
+import events.boudicca.openapi.model.Event
 import events.boudicca.openapi.model.SearchDTO
 import org.springframework.stereotype.Service
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
 
 @Service
 class EventService {
     private val publisherApi: EventPublisherResourceApi
+    private val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy 'um' HH:mm 'Uhr'")
 
     init {
         val apiClient = ApiClient()
@@ -17,12 +20,26 @@ class EventService {
         publisherApi = EventPublisherResourceApi(apiClient)
     }
 
-    fun getAllEvents(): Set<Event> {
-        return publisherApi.eventsGet().map{EventMapper.toEvent(it)}.toSet()
+    fun getAllEvents(): Set<Map<String, String?>> {
+        return publisherApi.eventsGet().map{map(it)}.toSet()
     }
 
-    fun search(searchDTO: SearchDTO): Set<Event> {
-        return publisherApi.eventsSearchPost(searchDTO).map{EventMapper.toEvent(it)}.toSet()
+    fun search(searchDTO: SearchDTO): Set<Map<String, String?>> {
+        return publisherApi.eventsSearchPost(searchDTO).map{map(it)}.toSet()
+    }
+
+    fun map(event: Event): Map<String, String?> {
+        return mapOf(
+                "name" to event.name,
+                "startDate" to formatDate(event.startDate),
+                "url" to (event.data?.get(SemanticKeys.URL) ?: ""),
+                "locationName" to (event.data?.get(SemanticKeys.LOCATION_NAME) ?: "unbekannt"),
+                "city" to (event.data?.get(SemanticKeys.LOCATION_CITY) ?: ""),
+        )
+    }
+
+    fun formatDate(startDate: OffsetDateTime): String {
+        return formatter.format(startDate);
     }
 
     private fun autoDetectUrl(): String {

4588f6ad6b7d7d212345e7efba23dd9d7184756e
Patrick Obermueller <pat.obermueller@gmail.com>
2023-05-07 21:20:24 +0200

    feature: icons, descriptions and colors update

--- publisher-html/src/main/kotlin/events/boudicca/publisherhtml/service/EventService.kt
+++ publisher-html/src/main/kotlin/events/boudicca/publisherhtml/service/EventService.kt
@@ -14,6 +14,11 @@ class EventService {
     private val publisherApi: EventPublisherResourceApi
     private val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy 'um' HH:mm 'Uhr'")
 
+    private val miscArtTypes = arrayOf("kabarett", "theater", "wissenskabarett", "provinzkrimi",
+            "comedy", "figurentheater", "film", "visual comedy", "tanz", "performance");
+    private val musicTypes = arrayOf("concert", "alternative", "singer/songwriter", "party", "songwriter/alternative");
+    private val techTypes = arrayOf("techmeetup");
+
     init {
         val apiClient = ApiClient()
         apiClient.updateBaseUri(autoDetectUrl())
@@ -21,11 +26,11 @@ class EventService {
     }
 
     fun getAllEvents(): Set<Map<String, String?>> {
-        return publisherApi.eventsGet().map{map(it)}.toSet()
+        return publisherApi.eventsGet().map { map(it) }.toSet()
     }
 
     fun search(searchDTO: SearchDTO): Set<Map<String, String?>> {
-        return publisherApi.eventsSearchPost(searchDTO).map{map(it)}.toSet()
+        return publisherApi.eventsSearchPost(searchDTO).map { map(it) }.toSet()
     }
 
     fun map(event: Event): Map<String, String?> {
@@ -36,7 +41,25 @@ class EventService {
                 "startDate" to formatDate(event.startDate),
                 "locationName" to (event.data?.get(SemanticKeys.LOCATION_NAME) ?: "unbekannt"),
                 "city" to event.data?.get(SemanticKeys.LOCATION_CITY),
+                "type" to mapType(event.data?.get(SemanticKeys.TYPE))
         )
+    }
+
+    fun mapType(type: String?): String? {
+        if (type === null) {
+            return null
+        }
+
+        val lowerCaseType = type.lowercase();
+        if (miscArtTypes.contains(lowerCaseType)) {
+            return "miscArt"
+        } else if (musicTypes.contains(lowerCaseType)) {
+            return "music"
+        } else if (techTypes.contains(lowerCaseType)) {
+            return "tech"
+        }
+
+        return null
     }
 
     fun formatDate(startDate: OffsetDateTime): String {

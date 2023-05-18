fca8189696b218698e046ecc7038219a22ae5c87
abl <ableimat@gmx.at>
2023-05-19 00:48:35 +0200

    also push search

--- build_and_push_images.sh
+++ build_and_push_images.sh
@@ -4,11 +4,13 @@
 
 #quarkus is stupid and does not allow me to make simple names
 docker tag registry-1.docker.io/library/boudicca-api registry.slothyx.com/boudicca-api
+docker tag registry-1.docker.io/library/boudicca-search registry.slothyx.com/boudicca-search
 docker tag boudicca-html registry.slothyx.com/boudicca-html
 docker tag registry-1.docker.io/library/boudicca-ical registry.slothyx.com/boudicca-ical
 docker tag boudicca-eventcollectors registry.slothyx.com/boudicca-eventcollectors
 
 docker push registry.slothyx.com/boudicca-api
+docker push registry.slothyx.com/boudicca-search
 docker push registry.slothyx.com/boudicca-html
 docker push registry.slothyx.com/boudicca-ical
 docker push registry.slothyx.com/boudicca-eventcollectors

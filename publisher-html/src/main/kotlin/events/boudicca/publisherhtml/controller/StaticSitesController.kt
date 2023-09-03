482d61f3ea5a2c947579a9335e7e2a07204852cf
abl <ableimat@gmx.at>
2023-09-03 14:05:50 +0200

    added impressum

--- publisher-html/src/main/kotlin/events/boudicca/publisherhtml/controller/StaticSitesController.kt
+++ publisher-html/src/main/kotlin/events/boudicca/publisherhtml/controller/StaticSitesController.kt
@@ -1,6 +1,5 @@
 package events.boudicca.publisherhtml.controller
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,15 +7,18 @@ import org.springframework.web.servlet.ModelAndView
 
 @Controller
 @RequestMapping("/")
-class AboutPageController @Autowired constructor() {
-
-    private val PAGE_TITLE = "About Boudicca";
+class StaticSitesController {
 
     @GetMapping("/about")
     fun getAbout(): ModelAndView {
         val data: MutableMap<String, Any> = HashMap()
-        data["title"] = PAGE_TITLE
         return ModelAndView("about", data)
+    }
+
+    @GetMapping("/impressum")
+    fun getImpressum(): ModelAndView {
+        val data: MutableMap<String, Any> = HashMap()
+        return ModelAndView("impressum", data)
     }
 }
 

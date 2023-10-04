95e837cb317de3238a4c7f30ea4e05ae2b15d672
Matthias Holzinger <yolgie@cnoize.at>
2023-10-04 03:01:53 +0200

    Bump kotlin version to 1.9.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.7.22" apply false
-    kotlin("plugin.allopen") version "1.7.22" apply false
-    kotlin("plugin.spring") version "1.8.22" apply false
+    kotlin("jvm") version "1.9.10" apply false
+    kotlin("plugin.allopen") version "1.9.10" apply false
+    kotlin("plugin.spring") version "1.9.10" apply false
 }

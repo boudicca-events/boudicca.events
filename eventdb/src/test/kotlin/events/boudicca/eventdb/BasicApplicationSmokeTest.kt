f20707598be10a8559b4c3cf6cf831bfde1bcdb1
Matthias Holzinger <matthias.holzinger@cloudflight.io>
2023-10-07 00:19:43 +0200

    remove force failing test again

--- eventdb/src/test/kotlin/events/boudicca/eventdb/BasicApplicationSmokeTest.kt
+++ eventdb/src/test/kotlin/events/boudicca/eventdb/BasicApplicationSmokeTest.kt
@@ -1,7 +1,6 @@
 package events.boudicca.eventdb
 
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.fail
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
@@ -11,10 +10,5 @@ class BasicApplicationSmokeTest {
     fun contextLoads() {
         // This test will automatically pass if the application context loads successfully.
         // If there's any bean misconfiguration or other Spring-related errors, it will fail.
-    }
-
-    @Test
-    fun failingTest() {
-        fail("fail smoketest")
     }
 }

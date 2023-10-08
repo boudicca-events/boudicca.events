4ac5e15396ca4bd44bc365afd13c8964210cc44c
abl <ableimat@gmx.at>
2023-10-08 11:16:51 +0200

    copy handlebars springmvc source so that we can change javax to jakarta and get rid of the migrate plugin

--- publisher-html/src/main/kotlin/events/boudicca/publisherhtml/PublisherHtmlApplication.kt
+++ publisher-html/src/main/kotlin/events/boudicca/publisherhtml/PublisherHtmlApplication.kt
@@ -1,6 +1,6 @@
 package events.boudicca.publisherhtml
 
-import com.github.jknack.handlebars.springmvc.HandlebarsViewResolver
+import events.boudicca.publisherhtml.handlebars.HandlebarsViewResolver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean

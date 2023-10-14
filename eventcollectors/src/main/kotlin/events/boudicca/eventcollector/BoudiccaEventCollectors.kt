1dcc823bd251dccd935fe4e4c655c989ef55372d
Paul Em <paul3m@gmail.com>
2023-10-15 00:08:23 +0200

    setup basic familienkarte event collector
    # Conflicts:
    #	boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/FamilienkarteEventCollector.kt
    #	eventcollectors/src/main/kotlin/events/boudicca/eventcollector/BoudiccaEventCollectors.kt

---
+++ eventcollectors/src/main/kotlin/events/boudicca/eventcollector/BoudiccaEventCollectors.kt
@@ -0,0 +1,32 @@
+package events.boudicca.eventcollector
+
+import events.boudicca.api.eventcollector.EventCollectorScheduler
+import events.boudicca.eventcollector.collectors.*
+
+fun main() {
+    Thread.sleep(10000) // let eventdb startup first when both are deployed.... we should do a better thing here
+    EventCollectorScheduler()
+        .startWebUi()
+//        .addEventCollector(LinzTermineCollector())
+//        .addEventCollector(PosthofCollector())
+//        .addEventCollector(JkuEventCollector())
+//        .addEventCollector(TechnologiePlauscherlCollector())
+//        .addEventCollector(ZuckerfabrikCollector())
+//        .addEventCollector(PlanetTTCollector())
+//        .addEventCollector(BrucknerhausCollector())
+//        .addEventCollector(OOESeniorenbundCollector())
+//        .addEventCollector(KupfTicketCollector())
+//        .addEventCollector(SpinnereiCollector())
+//        .addEventCollector(SchlachthofCollector())
+//        .addEventCollector(WissensturmCollector())
+//        .addEventCollector(LandestheaterLinzCollector())
+//        .addEventCollector(KapuCollector())
+//        .addEventCollector(StadtwerkstattCollector())
+//        .addEventCollector(InnovationsHauptplatzCodingWeeksCollector())
+//        .addEventCollector(OteloLinzCollector())
+//        .addEventCollector(EnnsEventsCollector())
+//        .addEventCollector(UlfOoeCollector())
+//        .addEventCollector(StiftskonzerteCollector())
+        .addEventCollector(FamilienkarteCollector())
+        .run()
+}

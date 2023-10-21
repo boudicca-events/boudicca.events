d632a03bd8ec1b59c078aabccca9d730312529ad
abl <ableimat@gmx.at>
2023-10-21 18:02:00 +0200

    change event class in eventdb

--- boudicca.base/eventdb/src/main/kotlin/base/boudicca/eventdb/model/Event.kt
+++ boudicca.base/eventdb/src/main/kotlin/base/boudicca/eventdb/model/Event.kt
@@ -1,13 +1,5 @@
 package base.boudicca.eventdb.model
 
-import java.time.ZonedDateTime
-
-data class Event(
-    val name: String,
-    val startDate: ZonedDateTime,
-    val data: Map<String, String>? = mapOf()
-)
-
 typealias EntryKey = Map<String, String>
 
 data class InternalEventProperties(
--- boudicca.base/eventdb/src/main/kotlin/base/boudicca/eventdb/service/EventSearchService.kt
+++ boudicca.base/eventdb/src/main/kotlin/base/boudicca/eventdb/service/EventSearchService.kt
@@ -1,7 +1,7 @@
 package base.boudicca.eventdb.service
 
+import base.boudicca.Event
 import base.boudicca.eventdb.model.ComplexSearchDto
-import base.boudicca.eventdb.model.Event
 import base.boudicca.eventdb.model.SearchDTO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -11,17 +11,15 @@ import org.springframework.stereotype.Service
 class EventSearchService @Autowired constructor(private val eventService: EventService) {
 
     fun search(searchDTO: SearchDTO): Set<Event> {
-        val fromDate = searchDTO.fromDate?.toZonedDateTime()
-        val toDate = searchDTO.toDate?.toZonedDateTime()
+        val fromDate = searchDTO.fromDate
+        val toDate = searchDTO.toDate
         return eventService.list()
             .filter { e -> fromDate == null || !e.startDate.isBefore(fromDate) }
             .filter { e -> toDate == null || !e.startDate.isAfter(toDate) }
             .filter { e ->
                 val data = e.data
                 searchDTO.name == null || e.name.lowercase().contains(searchDTO.name.lowercase())
-                        || (data != null && data.values.any {
-                    it.lowercase().contains(searchDTO.name.lowercase())
-                })
+                        || (data.values.any { it.lowercase().contains(searchDTO.name.lowercase()) })
             }
             .toSet()
     }
@@ -30,57 +28,57 @@ class EventSearchService @Autowired constructor(private val eventService: EventS
     fun searchBy(searchDto: ComplexSearchDto): Set<Event> {
         val filters = mutableSetOf<(Event) -> Boolean>()
         if (!searchDto.anyKeyExactMatch.isNullOrEmpty())
-            filters.add { event -> searchDto.anyKeyExactMatch.any { key -> event.data?.containsKey(key) ?: false } }
+            filters.add { event -> searchDto.anyKeyExactMatch.any { key -> event.data.containsKey(key) } }
         if (!searchDto.allKeyExactMatch.isNullOrEmpty())
-            filters.add { event -> searchDto.allKeyExactMatch.all { key -> event.data?.containsKey(key) ?: false } }
+            filters.add { event -> searchDto.allKeyExactMatch.all { key -> event.data.containsKey(key) } }
         if (!searchDto.anyKeyOrValueContains.isNullOrEmpty())
             filters.add { event ->
                 searchDto.anyKeyOrValueContains.any {
-                    event.data?.keys?.any { key -> key.contains(it.lowercase()) } ?: false ||
-                            event.data?.values?.any { value -> value.contains(it.lowercase()) } ?: false
+                    event.data.keys.any { key -> key.contains(it.lowercase()) } ||
+                            event.data.values.any { value -> value.contains(it.lowercase()) }
                 }
             }
         if (!searchDto.allKeyOrValueContains.isNullOrEmpty())
             filters.add { event ->
                 searchDto.allKeyOrValueContains.all {
-                    event.data?.keys?.any { key -> key.contains(it.lowercase()) } ?: false ||
-                            event.data?.values?.any { value -> value.contains(it.lowercase()) } ?: false
+                    event.data.keys.any { key -> key.contains(it.lowercase()) } ||
+                            event.data.values.any { value -> value.contains(it.lowercase()) }
                 }
             }
         if (!searchDto.anyKeyOrValueExactMatch.isNullOrEmpty())
             filters.add { event ->
                 searchDto.anyKeyOrValueExactMatch.any {
-                    event.data?.containsKey(it.lowercase()) ?: false || event.data?.containsValue(it.lowercase()) ?: false
+                    event.data.containsKey(it.lowercase()) || event.data.containsValue(it.lowercase())
                 }
             }
         if (!searchDto.allKeyOrValueExactMatch.isNullOrEmpty())
             filters.add { event ->
                 searchDto.allKeyOrValueExactMatch.all {
-                    event.data?.containsKey(it.lowercase()) ?: false || event.data?.containsValue(it.lowercase()) ?: false
+                    event.data.containsKey(it.lowercase()) || event.data.containsValue(it.lowercase())
                 }
             }
         if (!searchDto.anyValueForKeyContains.isNullOrEmpty())
             filters.add { event ->
                 searchDto.anyValueForKeyContains.any { (key, value) ->
-                    event.data?.get(key)?.lowercase()?.contains(value) ?: false
+                    event.data[key]?.lowercase()?.contains(value) ?: false
                 }
             }
         if (!searchDto.allValueForKeyContains.isNullOrEmpty())
             filters.add { event ->
                 searchDto.allValueForKeyContains.all { (key, value) ->
-                    event.data?.get(key)?.lowercase()?.contains(value) ?: false
+                    event.data[key]?.lowercase()?.contains(value) ?: false
                 }
             }
         if (!searchDto.anyValueForKeyExactMatch.isNullOrEmpty())
             filters.add { event ->
                 searchDto.anyValueForKeyExactMatch.any { (key, value) ->
-                    event.data?.get(key)?.lowercase()?.equals(value) ?: false
+                    event.data[key]?.lowercase()?.equals(value) ?: false
                 }
             }
         if (!searchDto.allValueForKeyExactMatch.isNullOrEmpty())
             filters.add { event ->
                 searchDto.allValueForKeyExactMatch.all { (key, value) ->
-                    event.data?.get(key)?.lowercase()?.equals(value) ?: false
+                    event.data[key]?.lowercase()?.equals(value) ?: false
                 }
             }
 

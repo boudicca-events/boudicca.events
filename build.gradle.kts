f3684c981c36c2e2aa11821b5a7a16c64065dfc3
abl <ableimat@gmx.at>
2024-01-10 20:44:03 +0100

    update all the things

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
-    kotlin("kapt") version "1.9.21"
-    id("io.cloudflight.autoconfigure.swagger-api-configure") version "1.1.0" apply false
-    kotlin("plugin.allopen") version "1.9.21" apply false
-    kotlin("plugin.spring") version "1.9.21" apply false
+    kotlin("jvm") version "1.9.22"
+    kotlin("kapt") version "1.9.22"
+    id("io.cloudflight.autoconfigure.swagger-api-configure") version "1.1.1" apply false
+    kotlin("plugin.allopen") version "1.9.22" apply false
+    kotlin("plugin.spring") version "1.9.22" apply false
     id("org.springframework.boot") version "3.2.0" apply false
     id("io.spring.dependency-management") version "1.1.4" apply false
 }

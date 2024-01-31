4f575c2f57b5711d503063e7b1e0e7fce2869740
Daniel Morawetz <daniel@morawetz.dev>
2024-01-31 14:16:00 +0100

    Add a new collector for valug.at
    VorAlpen Linux User Group is a local Linux user community that meets
    regularly. Most events happen at the Klubraum. If the ical event
    location contains this keyword, further geolocation is added.

---
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/ValugCollector.kt
@@ -0,0 +1,75 @@
+package events.boudicca.eventcollector.collectors
+
+import base.boudicca.SemanticKeys
+import base.boudicca.api.eventcollector.Fetcher
+import base.boudicca.api.eventcollector.TwoStepEventCollector
+import base.boudicca.model.Event
+import base.boudicca.model.EventCategory
+import net.fortuna.ical4j.data.CalendarBuilder
+import net.fortuna.ical4j.model.Calendar
+import net.fortuna.ical4j.model.component.VEvent
+import java.io.StringReader
+import java.net.URI
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+/**
+ * VorAlpen Linux User Group
+ */
+class ValugCollector : TwoStepEventCollector<VEvent>("valug") {
+    private val fetcher = Fetcher()
+    private val baseUrl = "https://valug.at/"
+    private val icsUrl = "${baseUrl}events/index.ics"
+
+    override fun getAllUnparsedEvents(): List<VEvent> {
+        val feed = fetcher.fetchUrl(icsUrl)
+        val builder = CalendarBuilder()
+        val calendar: Calendar = builder.build(StringReader(feed))
+        return calendar.components.filterIsInstance<VEvent>()
+    }
+
+    override fun parseEvent(event: VEvent): Event? {
+        val eventName = event.summary.value
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss")
+        // get by city name to account for daylight saving time
+        val viennaZoneId = TimeZone.getTimeZone("Europe/Vienna").toZoneId()
+        val eventStartDate = LocalDateTime.parse(event.startDate.value, formatter)
+            .atZone(viennaZoneId).toOffsetDateTime()
+        val now = LocalDateTime.now().atZone(viennaZoneId).toOffsetDateTime()
+
+        if (eventStartDate.isBefore(now)) {
+            return null
+        }
+
+        val eventEndDate = LocalDateTime.parse(event.startDate.value, formatter)
+            .atZone(TimeZone.getTimeZone("Europe/Vienna").toZoneId()).toOffsetDateTime()
+
+        return Event(
+            eventName, eventStartDate,
+            buildMap {
+                put(SemanticKeys.ENDDATE, eventEndDate.format(DateTimeFormatter.ISO_DATE_TIME))
+                if (event.location != null) {
+                    put(SemanticKeys.LOCATION_NAME, event.location.value)
+                    if (event.location.value.contains("VALUG-Klubraum")) {
+                        put(SemanticKeys.LOCATION_CITY, "Wels")
+                        put(SemanticKeys.LOCATION_URL, "https://valug.at/pages/Klubraum/")
+                        put(SemanticKeys.LOCATION_ADDRESS, "Dragonerstraße 22, 4600 Wels")
+                        put(SemanticKeys.LOCATION_COORDINATES, "14.01715, 48.15849")
+                    }
+                }
+                if (event.description != null) {
+                    put(SemanticKeys.DESCRIPTION, event.description.value)
+                }
+                put(SemanticKeys.TAGS, listOf("VALUG", "Linux", "User Group").toString())
+                put(SemanticKeys.URL, event.url.value)
+                put(SemanticKeys.TYPE, "techmeetup") // TODO same as with Technologieplauscherl
+                put(SemanticKeys.CATEGORY, EventCategory.TECH.name)
+                put(SemanticKeys.REGISTRATION, "free")
+                put("url.ics", icsUrl)
+                put("valug.uid", event.uid.value)
+                put(SemanticKeys.SOURCES, "${icsUrl}\n${baseUrl}")
+            }
+        )
+    }
+}

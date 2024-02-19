1a713377347953b65237ac224dd9e688778d6d51
abl <ableimat@gmx.at>
2024-02-19 18:22:40 +0100

    remove all other stuff

--- search/src/main/kotlin/events/boudicca/search/model/Event.kt
+++
@@ -1,9 +0,0 @@
-package events.boudicca.search.model
-
-import java.time.ZonedDateTime
-
-data class Event(
-        val name: String,
-        val startDate: ZonedDateTime,
-        val data: Map<String, String>? = mapOf()
-)
--- search/src/main/kotlin/events/boudicca/search/model/QueryDTO.kt
+++
@@ -1,7 +0,0 @@
-package events.boudicca.search.model
-
-data class QueryDTO(
-    val query: String? = null,
-    val offset: Int? = null,
-    val size: Int? = null,
-)

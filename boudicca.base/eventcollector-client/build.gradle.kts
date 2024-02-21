25ba2c2079d6b3e7c9936977fcf492adb4f1d8ad
abl <ableimat@gmx.at>
2024-02-21 23:11:55 +0100

    cleanup gradle file to publish eventcollectors and add environment and systemproperty checks for loading config in eventcollectors

--- boudicca.base/eventcollector-client/build.gradle.kts
+++ boudicca.base/eventcollector-client/build.gradle.kts
@@ -1,40 +1,15 @@
 plugins {
-    kotlin("jvm")
-    kotlin("plugin.allopen")
-    `maven-publish`
-}
-
-repositories {
-    mavenCentral()
-    mavenLocal()
-}
-
-kotlin {
-    jvmToolchain(rootProject.ext["jvmVersion"] as Int)
-    compilerOptions {
-        javaParameters = true
-    }
+    id("boudicca-kotlin")
+    id("boudicca-publish")
 }
 
 dependencies {
-    api("org.jetbrains.kotlin:kotlin-stdlib")
+    api(project(":boudicca.base:semantic-conventions"))
     implementation("org.apache.velocity:velocity-engine-core:2.3")
     implementation("org.apache.velocity.tools:velocity-tools-generic:3.1")
     implementation("ch.qos.logback:logback-classic:1.5.0")
-    api("org.slf4j:slf4j-api:2.0.12")
+    implementation("org.slf4j:slf4j-api:2.0.12")
     implementation(project(":boudicca.base:ingest-client"))
     implementation(project(":boudicca.base:enricher-client"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("eventcollector-client") {
-            from(components["java"])
-        }
-    }
 }

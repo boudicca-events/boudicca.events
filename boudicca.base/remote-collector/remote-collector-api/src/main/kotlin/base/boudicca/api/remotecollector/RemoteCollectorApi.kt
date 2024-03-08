3a91789d2e4a1484bc09f7f4583e8e1b9646a35a
abl <ableimat@gmx.at>
2024-03-08 12:40:58 +0100

    remote collector api

---
+++ boudicca.base/remote-collector/remote-collector-api/src/main/kotlin/base/boudicca/api/remotecollector/RemoteCollectorApi.kt
@@ -0,0 +1,28 @@
+package base.boudicca.api.remotecollector
+
+import base.boudicca.api.remotecollector.model.EventCollection
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import javax.ws.rs.GET
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+
+@OpenAPIDefinition
+interface RemoteCollectorApi {
+    @Operation(
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "trigger a EventCollection",
+                useReturnTypeSchema = true
+            )
+        ],
+        tags = ["remote-collector"]
+    )
+    @GET
+    @Path("collectEvents")
+    @Produces("application/json")
+    fun collectEvents(): EventCollection
+}
---
+++ boudicca.base/remote-collector/remote-collector-client/src/main/kotlin/base/boudicca/api/remotecollector/RemoteCollectorClient.kt
@@ -0,0 +1,57 @@
+package base.boudicca.api.remotecollector
+
+import base.boudicca.api.remotecollector.model.EventCollection
+import base.boudicca.api.remotecollector.model.HttpCall
+import base.boudicca.model.Event
+import base.boudicca.openapi.ApiClient
+import base.boudicca.openapi.ApiException
+import base.boudicca.remote_collector.openapi.api.RemoteCollectorApi
+
+class RemoteCollectorClient(private val remoteCollectorUrl: String) {
+
+    private val remoteCollectorApi: RemoteCollectorApi
+
+    init {
+        if (remoteCollectorUrl.isBlank()) {
+            throw IllegalStateException("you need to pass an remoteCollectorUrl!")
+        }
+        val apiClient = ApiClient()
+        apiClient.updateBaseUri(remoteCollectorUrl)
+
+        remoteCollectorApi = RemoteCollectorApi(apiClient)
+    }
+
+    fun collectEvents(): EventCollection {
+        try {
+            return toApi(remoteCollectorApi.collectEvents())
+        } catch (e: ApiException) {
+            throw RemoteCollectorException("could not reach remote collector: $remoteCollectorUrl", e)
+        }
+    }
+
+    private fun toApi(eventCollection: base.boudicca.remote_collector.openapi.model.EventCollection): EventCollection {
+        return EventCollection(
+            eventCollection.events?.map { toApi(it) } ?: emptyList(),
+            eventCollection.httpCalls?.map { toApi(it) },
+            eventCollection.logLines,
+            eventCollection.warningCount,
+            eventCollection.errorCount,
+        )
+    }
+
+    private fun toApi(event: base.boudicca.remote_collector.openapi.model.Event): Event {
+        return Event(event.name!!, event.startDate!!, event.data ?: emptyMap())
+    }
+
+    private fun toApi(httpCall: base.boudicca.remote_collector.openapi.model.HttpCall): HttpCall {
+        return HttpCall(
+            httpCall.url!!,
+            httpCall.responseCode!!,
+            httpCall.startTime!!,
+            httpCall.endTime!!,
+            httpCall.postParams
+        )
+    }
+}
+
+class RemoteCollectorException(msg: String, e: ApiException) : RuntimeException(msg, e)

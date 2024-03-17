79394914a3b107ae11e6228d9a0b5e87a92e209a
abl <ableimat@gmx.at>
2024-03-17 17:55:45 +0100

    adapt deploy_images

--- deploy_images.sh
+++ deploy_images.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
 
-docker tag localhost/boudicca-eventdb ghcr.io/boudicca-events/eventdb
-docker tag localhost/boudicca-enricher ghcr.io/boudicca-events/enricher
-docker tag localhost/boudicca-search ghcr.io/boudicca-events/search
-docker tag localhost/boudicca-events-publisher-event-html ghcr.io/boudicca-events/publisher-event-html
-docker tag localhost/boudicca-publisher-event-ical ghcr.io/boudicca-events/publisher-event-ical
-docker tag localhost/boudicca-events-eventcollectors ghcr.io/boudicca-events/eventcollectors
+docker tag localhost/boudicca-events-boudicca-html-diabolical registry.slothyx.com/boudicca-html-diabolical
 
-echo "pushing eventdb"
-docker push ghcr.io/boudicca-events/eventdb
-echo "pushing enricher"
-docker push ghcr.io/boudicca-events/enricher
-echo "pushing search"
-docker push ghcr.io/boudicca-events/search
-echo "pushing html"
-docker push ghcr.io/boudicca-events/publisher-event-html
-echo "pushing ical"
-docker push ghcr.io/boudicca-events/publisher-event-ical
-echo "pushing eventcollectors"
-docker push ghcr.io/boudicca-events/eventcollectors
+echo "pushing diabolical"
+docker push registry.slothyx.com/boudicca-html-diabolical

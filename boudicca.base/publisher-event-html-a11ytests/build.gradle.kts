e8e9113ca747350e7570180a41d49b94ec5f1268
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-03-25 14:34:34 +0100

    add `playwright`, `axe-core` and `junit` as dependencies

--- boudicca.base/publisher-event-html-a11ytests/build.gradle.kts
+++ boudicca.base/publisher-event-html-a11ytests/build.gradle.kts
@@ -2,3 +2,9 @@ plugins {
     id("boudicca-kotlin")
 }
 
+dependencies {
+    api("com.microsoft.playwright:playwright:1.42.0")
+    api("com.deque.html.axe-core:playwright:4.8.2")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+}

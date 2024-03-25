64a233d8a976791ae1e8d0cddf102b717cb30c04
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-03-25 12:26:45 +0100

    remove extra configurations and use `boudicca-kotlin` plugin instead

--- boudicca.base/publisher-event-html-a11ytests/build.gradle.kts
+++ boudicca.base/publisher-event-html-a11ytests/build.gradle.kts
@@ -1,21 +1,4 @@
 plugins {
-    kotlin("jvm") version "1.9.22"
+    id("boudicca-kotlin")
 }
 
-group = "org.example"
-version = "0.4.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
-}

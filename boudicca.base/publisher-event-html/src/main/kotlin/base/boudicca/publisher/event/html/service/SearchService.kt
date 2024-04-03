5fa1ef9b41e356ba9d9e89a1476529c0ed4b0220
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-04-03 22:37:00 +0200

    add `SearchService` class and SearchServiceCaller interface

---
+++ boudicca.base/publisher-event-html/src/main/kotlin/base/boudicca/publisher/event/html/service/SearchService.kt
@@ -0,0 +1,25 @@
+package base.boudicca.publisher.event.html.service
+
+import base.boudicca.api.search.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@Service
+class SearchService @Autowired constructor(
+  @Value("\${boudicca.search.url}") private val searchUrl: String,
+): SearchServiceCaller {
+  val client: SearchClient = createSearchClient(searchUrl)
+
+  override fun queryEvents(queryDTO: QueryDTO): SearchResultDTO {
+    return client.queryEvents(queryDTO)
+  }
+
+  override fun getFiltersFor(filterQueryDTO: FilterQueryDTO): FilterResultDTO {
+    return client.getFiltersFor(filterQueryDTO)
+  }
+
+  fun createSearchClient(searchUrl: String): SearchClient {
+    return SearchClient(searchUrl)
+  }
+}

2ecfdbcbe4f855a69282a83391e4315a3107052f
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-04-08 19:15:08 +0200

    add `E2ETestFixture` class

---
+++ boudicca.base/publisher-event-html/src/test/kotlin/base/boudicca/publisher/event/html/E2ETestFixture.kt
@@ -0,0 +1,34 @@
+package base.boudicca.publisher.event.html
+
+import com.microsoft.playwright.*
+import org.junit.jupiter.api.*
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+open class E2ETestFixture {
+  var playwright: Playwright = Playwright.create()
+
+  lateinit var browser: Browser
+  lateinit var context: BrowserContext
+  lateinit var page: Page
+
+  @BeforeAll
+  fun launchBrowser() {
+    browser = playwright.chromium().launch(BrowserType.LaunchOptions().setHeadless(false))
+  }
+
+  @BeforeEach
+  fun createContextAndPage() {
+    context = browser.newContext()
+    page = context.newPage()
+  }
+
+  @AfterAll
+  fun closeBrowser() {
+    playwright.close()
+  }
+
+  @AfterEach
+  fun closeContext() {
+    context.close()
+  }
+}

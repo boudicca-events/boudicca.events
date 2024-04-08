7d4d1fe45fa26f69a17a811aeb15c7bba0b4da41
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-04-08 18:36:54 +0200

    automatically detect violations in `drawer`

--- boudicca.base/publisher-event-html/src/test/kotlin/base/boudicca/publisher/event/html/PublisherHtmlApplicationA11YTests.kt
+++ boudicca.base/publisher-event-html/src/test/kotlin/base/boudicca/publisher/event/html/PublisherHtmlApplicationA11YTests.kt
@@ -10,25 +10,21 @@ import com.microsoft.playwright.Browser
 import com.microsoft.playwright.BrowserContext
 import com.microsoft.playwright.Page
 import com.microsoft.playwright.Playwright
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Tag
-import org.junit.jupiter.api.TestInstance
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
-import org.springframework.boot.test.web.server.LocalServerPort
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import org.mockito.kotlin.any
 import org.mockito.kotlin.whenever
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.time.OffsetDateTime
+import java.util.*
 import java.util.stream.Stream
 
 
@@ -87,6 +83,31 @@ class PublisherHtmlApplicationA11YTests {
     page.navigate("http://localhost:$port/")
 
     val accessibilityScanResults: AxeResults = AxeBuilder(page).analyze()
+
+    assertEquals(listOf<Any>(), accessibilityScanResults.violations)
+  }
+
+  @ParameterizedTest
+  @MethodSource("testData")
+  fun drawerShouldNotHaveAutomaticallyDetectableAccessibilityViolations(
+    events: List<Event>,
+    filters: Map<String, List<String>>
+  ) {
+    whenever(searchServiceCaller!!.search(any())).thenReturn(SearchResultDTO(events, 1, null))
+    whenever(searchServiceCaller!!.getFiltersFor(any())).thenReturn(filters)
+
+    page.navigate("http://localhost:$port/")
+
+    page.locator("button[id=\"filterButton\"]").click()
+
+    // It is important to waitFor() the page to be in the desired
+    // state *before* running analyze(). Otherwise, axe might not
+    // find all the elements your test expects it to scan.
+    page.locator("#drawer").waitFor()
+
+    val accessibilityScanResults: AxeResults = AxeBuilder(page)
+      .include(Arrays.asList("#drawer"))
+      .analyze()
 
     assertEquals(listOf<Any>(), accessibilityScanResults.violations)
   }

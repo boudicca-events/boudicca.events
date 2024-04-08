44197e5a36e8990abe7ab6014112877d2068a0ae
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-04-08 14:58:32 +0200

    rename `SearchService` to `SearchServiceCallerImpl`

--- boudicca.base/publisher-event-html/src/main/kotlin/base/boudicca/publisher/event/html/service/SearchServiceCallerImpl.kt
+++ boudicca.base/publisher-event-html/src/main/kotlin/base/boudicca/publisher/event/html/service/SearchServiceCallerImpl.kt
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class SearchService @Autowired constructor(
+class SearchServiceCallerImpl @Autowired constructor(
   @Value("\${boudicca.search.url}") private val searchUrl: String,
 ): SearchServiceCaller {
   private val client: SearchClient = createSearchClient(searchUrl)

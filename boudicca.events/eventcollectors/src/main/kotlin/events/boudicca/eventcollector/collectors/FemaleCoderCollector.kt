71941f849db6e0ba8d781b1881643fc1b9fc9d99
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-04-23 23:18:01 +0200

    get all unparsed events

--- boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/FemaleCoderCollector.kt
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/FemaleCoderCollector.kt
@@ -1,9 +1,31 @@
 package events.boudicca.eventcollector.collectors
 
+import base.boudicca.api.eventcollector.Fetcher
 import base.boudicca.api.eventcollector.TwoStepEventCollector
+import base.boudicca.model.Event
+import org.jsoup.Jsoup
+import org.slf4j.LoggerFactory
 
 class FemaleCoderCollector : TwoStepEventCollector<String>("femalecoder") {
+
+  private val LOG = LoggerFactory.getLogger(this::class.java)
+  private val fetcher = Fetcher()
+  private val baseUrl = "https://female-coders.at/"
+
   override fun getAllUnparsedEvents(): List<String>? {
-    TODO("Not yet implemented")
+    val eventUrls = mutableListOf<String>()
+
+    val document = Jsoup.parse(fetcher.fetchUrl(baseUrl))
+
+    document.select("div.et_pb_section_3 .event_details")
+      .forEach {
+        eventUrls.add(it.select("a").first().attr("href"))
+      }
+
+    return eventUrls
+  }
+
+  override fun parseEvent(event: String): Event? {
+    TODO()
   }
 }

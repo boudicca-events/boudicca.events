21ba0868973c4fed76e45508ced6f2b239f9f339
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-04-23 21:28:05 +0200

    parse all events

--- boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/MetalCornerCollector.kt
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/MetalCornerCollector.kt
@@ -4,21 +4,23 @@ import base.boudicca.SemanticKeys
 import base.boudicca.api.eventcollector.Fetcher
 import base.boudicca.api.eventcollector.TwoStepEventCollector
 import base.boudicca.model.Event
+import base.boudicca.model.EventCategory
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.slf4j.LoggerFactory
-import java.time.LocalDate
-import java.time.OffsetDateTime
-import java.time.ZoneId
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
-class MetalCornerCollector : TwoStepEventCollector<Pair<Pair<String, String>, Document>>("metalcorner") {
+class MetalCornerCollector : TwoStepEventCollector<Triple<String, String, Document>>("metalcorner") {
 
   private val LOG = LoggerFactory.getLogger(this::class.java)
   private val baseUrl = "https://www.escape-metalcorner.at/"
 
-  override fun getAllUnparsedEvents(): List<Pair<Pair<String, String>, Document>> {
+  override fun getAllUnparsedEvents(): List<Triple<String, String, Document>> {
     val fetcher = Fetcher()
-    val events = mutableListOf<Pair<Pair<String, String>, Document>>()
+
+    val events = mutableListOf<Triple<String, String, Document>>()
     val eventUrls = mutableListOf<Pair<String, String>>()
 
     val document = Jsoup.parse(fetcher.fetchUrl(baseUrl + "de/events"))
@@ -29,27 +31,34 @@ class MetalCornerCollector : TwoStepEventCollector<Pair<Pair<String, String>, Do
 
     eventUrls.forEach {
       val url = baseUrl + it.second.substring(2)
-      val eventTitle = it.first
-      val document = Jsoup.parse(fetcher.fetchUrl(url))
-      events.add(Pair(Pair(eventTitle, url), document))
+      val eventType = it.first
+      events.add(Triple(eventType, url, Jsoup.parse(fetcher.fetchUrl(url))))
     }
 
     return events
   }
 
-  override fun parseEvent(event: Pair<Pair<String, String>, Document>): Event {
-    val (eventUrl, doc) = event
-    val (title, url) = eventUrl
+  override fun parseEvent(event: Triple<String, String, Document>): Event {
+    val (eventType, url, doc) = event
     val data = mutableMapOf<String, String>()
     data[SemanticKeys.URL] = url
+    data[SemanticKeys.TYPE] = eventType
+    data[SemanticKeys.NAME] = "Escape - " + doc.select("div#content h1").text()
+    data[SemanticKeys.PICTURE_URL] = doc.select("div#content img").attr("src")
+    data[SemanticKeys.PICTURE_ALT_TEXT] = doc.select("div#content img").attr("alt")
+    data[SemanticKeys.SOURCES] = data[SemanticKeys.URL]!!
+    data[SemanticKeys.LOCATION_NAME] = "Escape Metalcorner"
+    data[SemanticKeys.LOCATION_CITY] = "Wien"
+    data[SemanticKeys.LOCATION_ADDRESS] = "Escape Metalcorner, Neustiftgasse 116-118, 1070 Wien"
+    data[SemanticKeys.LOCATION_URL] = "https://www.escape-metalcorner.at/"
+    data[SemanticKeys.REGISTRATION] = "ticket"
+    data[SemanticKeys.CATEGORY] = EventCategory.MUSIC.name
 
-    LOG.info(title)
-    LOG.info(doc.text())
+    val formatter = DateTimeFormatter.ofPattern("EEEE, dd.MM.yyyy HH:mm", Locale.GERMAN)
+    val zoneId = TimeZone.getTimeZone("Europe/Vienna").toZoneId()
+    val eventStartDate = LocalDateTime.parse(doc.select("div#content h2").text(), formatter)
+      .atZone(zoneId).toOffsetDateTime()
 
-    LOG.info(data.toString())
-
-    val startDate = LocalDate.now(ZoneId.of("Europe/Vienna")) as OffsetDateTime
-
-    return Event(name = "my event", startDate, data)
+    return Event(name = "my event", eventStartDate, data)
   }
 }

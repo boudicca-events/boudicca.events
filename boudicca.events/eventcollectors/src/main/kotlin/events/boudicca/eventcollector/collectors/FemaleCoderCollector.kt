f3bfbb19c5ae5f4dc0dcfa028e9b7e6d2e2898f3
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-04-24 13:37:29 +0200

    wip: parse events

--- boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/FemaleCoderCollector.kt
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/FemaleCoderCollector.kt
@@ -4,9 +4,12 @@ import base.boudicca.SemanticKeys
 import base.boudicca.api.eventcollector.Fetcher
 import base.boudicca.api.eventcollector.TwoStepEventCollector
 import base.boudicca.model.Event
+import base.boudicca.model.EventCategory
 import org.jsoup.Jsoup
 import org.slf4j.LoggerFactory
-import java.time.LocalDateTime
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.*
 
@@ -23,7 +26,7 @@ class FemaleCoderCollector : TwoStepEventCollector<String>("femalecoder") {
 
     document.select("div.et_pb_section_3 .event_details")
       .forEach {
-        it.select("a").first()?.let { it1 -> eventUrls.add(it1.attr("href")) }
+        eventUrls.add(it.select("a").first().attr("href"))
       }
 
     return eventUrls
@@ -44,26 +47,36 @@ class FemaleCoderCollector : TwoStepEventCollector<String>("femalecoder") {
 
     val description = StringBuilder()
 
+    // adding up all the p tags which are in the description area and ignore does under "organizermain" locator
     paragraphs.forEach {
       if (it.parents().select("organizermain").isEmpty()) {
         description.append(it.text()).append("\n")
       }
     }
     data[SemanticKeys.DESCRIPTION] = description.toString()
+
     data[SemanticKeys.SOURCES] = data[SemanticKeys.URL]!!
 
+    val date = document.select("div.details p").first()?.text()
+    val startTime = document.select("div.details p").last()?.text()?.substring(0, 8)!!
 
-    val unformattedDate = document.select("h1.entry-title").text().substring(21)
-    val unformattedTime = document.select("div.details p").last()?.text()?.substring(0, 8)
+    val currentYear = LocalDate.now().year
 
-    val unformattedDateAndTime = unformattedTime + " - " + unformattedDate
+    val localDate = LocalDate.parse("$date $currentYear", DateTimeFormatter.ofPattern("MMMM dd yyyy", Locale.ENGLISH))
+    val localStartTime = LocalTime.parse(startTime.uppercase(Locale.getDefault()), DateTimeFormatter.ofPattern("hh:mm a"))
 
-    // TODO: fix it, can not parse!
-    val formatter = DateTimeFormatter.ofPattern("HH:mm a - dd.MM.yyyy", Locale.GERMAN)
-    val zoneId = TimeZone.getTimeZone("Europe/Vienna").toZoneId()
-    val eventStartDate = LocalDateTime.parse(unformattedDateAndTime, formatter)
-      .atZone(zoneId).toOffsetDateTime()
+    val offsetDateTime = localDate.atTime(localStartTime)
+      .atZone(ZoneId.of("Europe/Vienna"))
+      .toOffsetDateTime()
 
-    return Event(name, eventStartDate, data)
+    data[SemanticKeys.PICTURE_URL] = document.select("div.container > div#content-area img").attr("href")
+    data[SemanticKeys.TYPE] = "technology"
+    data[SemanticKeys.CATEGORY] = EventCategory.TECH.name
+    data[SemanticKeys.TAGS] = listOf("Study Group", "Coding", "Mentorship", "TechCommunity", "Socializing", "Networking").toString()
+
+    data[SemanticKeys.LOCATION_NAME] = document.select("div.venue > p").first()?.text()!!
+//    data[SemanticKeys.LOCATION_CITY] =
+
+    return Event(name, offsetDateTime, data)
   }
 }

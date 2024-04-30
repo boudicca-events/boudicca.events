30edab4e3150730213699ed0ab68181f1548c299
Mahdi Khashan <mahdikhashan1@gmail.com>
2024-04-30 13:19:53 +0200

    a11y: change page title

--- boudicca.base/publisher-event-html/src/main/kotlin/base/boudicca/publisher/event/html/controller/StartPageController.kt
+++ boudicca.base/publisher-event-html/src/main/kotlin/base/boudicca/publisher/event/html/controller/StartPageController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.servlet.ModelAndView
 @RequestMapping("/")
 class StartPageController @Autowired constructor(private val eventService: EventService) {
 
-    private val PAGE_TITLE = "Boudicca"
+    private val PAGE_TITLE = "Boudicca.Events - Open source event collector and publisher software"
 
     @GetMapping("/")
     fun getIndex(): ModelAndView {

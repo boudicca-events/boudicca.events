3822bb32618bf729c71ebe078fecc1bff9461c9c
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2024-05-28 22:37:23 +0200

    wip: manual testing of html publisher w.r.t WCAG 2.1 (#423)
    * wip: manual testing of html publisher w.r.t WCAG 2.1
    
    * set background color for every focusable element
    
    * set background color only when keyboard triggered it
    
    * rename skip link text
    
    * close drawer on `Escape` key press when it's open
    
    * fix text-spacing for event-details
    
    * improve target-size for filter button (open drawer)
    
    * disable border-bottom to align it with close elements
    
    * add document for manual testing
    
    * wip: accessibility statement page
    
    * add accessibility statement link to footer
    
    * use `color-main-yellow-focus` for skip link background
    
    * prevent hardcoded title and use it from controller
    
    * add skip link to the source page
    
    * update `pageShouldHaveCorrectTitle` test
    
    * wip: accessibility statement
    
    * wip: accessibility statement
    
    * wip: accessibility statement
    
    * remove the link for accessibility statement in header
    
    * open the link for accessibility statement in the current browser
    
    * add skip links to all pages
    
    * fix: search field return flow
    
    * wip: event element screen reader and focus improvement
    
    * revert changes for tabindex of event elements
    
    * format code
    
    * add return link to content called "nach oben"
    
    * remove `firefox` and `safari` from tested browser in the accessibility statement page
    
    * capitalize `nach oben`
    
    * wip: focus on first added element
    
    * add feedback section
    
    * add kontakt information
    
    * state color contrast conflict with wcag aa
    
    * set focus to events-page on pressing on load more button
    
    * format `feedback` section
    
    * fix search field keyword notfound info and end of result info
    
    * change Sie to Du
    
    * remove commented code
    
    * revert endOfResults section text

--- boudicca.base/publisher-event-html/src/main/resources/static/js/main.js
+++ boudicca.base/publisher-event-html/src/main/resources/static/js/main.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const loadMoreButton = document.getElementById("loadMoreButton");
   const categorySelect = document.getElementById("category");
   const categoryFieldSets = document.querySelectorAll("[data-category-wanted]");
+  const searchInput = document.querySelector("input.search-input");
 
   loadMoreButton.addEventListener("click", () => {
     onLoadMoreSearch();
@@ -70,23 +71,67 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  const onSearchLoadMoreButtonBehaviour = (response) => {
+  const onSearchButtonBehaviour = (rawResponse) => {
     const loadMoreButton = document.getElementById("loadMoreButton");
     const endOfResultsInfo = document.getElementById("endOfResults");
+    const resultNotFoundLink = document.getElementById("resultNotFound");
+    const searchText = document.getElementById("searchText");
+    const searchInputKeyword = searchInput.value;
+
     if (!loadMoreButton || !endOfResultsInfo) {
       return;
     }
 
+    const response = rawResponse.trim()
     if (!response) {
-      if (loadMoreButton) {
-        loadMoreButton.style.display = "none";
+      loadMoreButton.style.display = "none";
+      endOfResultsInfo.style.display = "block";
+
+      if (searchInputKeyword === "") {
+        resultNotFoundLink.style.display = "none";
+      } else {
+        searchText.textContent = `"${searchInputKeyword}"`;
+        // in userflow from search field path, show the result not found link and hide the end of result
+        resultNotFoundLink.style.display = "block";
+        endOfResultsInfo.style.display = "none";
+      }
+    } else {
+      loadMoreButton.style.display = "inline-block";
+      endOfResultsInfo.style.display = "none";
+      resultNotFoundLink.style.display = "none";
+    }
+  }
+
+  const onLoadMoreButtonBehaviour = (rawResponse) => {
+    const loadMoreButton = document.getElementById("loadMoreButton");
+    const endOfResultsInfo = document.getElementById("endOfResults");
+    const resultNotFoundLink = document.getElementById("resultNotFound");
+    const searchText = document.getElementById("searchText");
+    const searchInputKeyword = searchInput.value;
+
+    if (!loadMoreButton || !endOfResultsInfo) {
+      return;
+    }
+
+    const response = rawResponse.trim()
+    if (!response) {
+      loadMoreButton.style.display = "none";
+      endOfResultsInfo.style.display = "block";
+
+      if (searchInputKeyword === "") {
+        resultNotFoundLink.style.display = "none";
+      } else {
+        searchText.textContent = `"${searchInputKeyword}"`;
+        // in userflow from search field path, hide the result not found link and show the end of result
+        resultNotFoundLink.style.display = "none";
         endOfResultsInfo.style.display = "block";
       }
     } else {
       loadMoreButton.style.display = "inline-block";
       endOfResultsInfo.style.display = "none";
+      resultNotFoundLink.style.display = "none";
     }
-  };
+  }
 
   const onSearch = async (e) => {
     e.preventDefault();
@@ -99,7 +144,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const response = await fetch(apiUrl);
       const ssrDomEventString = await response.text();
       eventsContainer.innerHTML = ssrDomEventString;
-      onSearchLoadMoreButtonBehaviour(ssrDomEventString);
+      onSearchButtonBehaviour(ssrDomEventString);
       goTo(`/search?${paramsAsString}`);
     } catch (e) {
       console.error(e);
@@ -117,8 +162,9 @@ document.addEventListener("DOMContentLoaded", () => {
       const response = await fetch(apiUrl);
       const ssrDomEventString = await response.text();
 
-      onSearchLoadMoreButtonBehaviour(ssrDomEventString);
+      onLoadMoreButtonBehaviour(ssrDomEventString);
       const newEvents = parser.parseFromString(ssrDomEventString, "text/html");
+
       eventsContainer.append(...newEvents.body.children);
       goTo(`/search?${paramsAsString}`);
     } catch (e) {

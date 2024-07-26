6855fd3859f3761e742ce1b34f42cc922ec45504
abl <ableimat@gmx.at>
2024-07-26 12:42:33 +0200

    update all the things again

--- boudicca.base/publisher-event-html/build.gradle.kts
+++ boudicca.base/publisher-event-html/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     api("com.github.jknack:handlebars:4.4.0")
     api(project(":boudicca.base:search-client"))
-    testImplementation("com.microsoft.playwright:playwright:1.45.0")
+    testImplementation("com.microsoft.playwright:playwright:1.45.1")
     testImplementation("com.deque.html.axe-core:playwright:4.9.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.3")

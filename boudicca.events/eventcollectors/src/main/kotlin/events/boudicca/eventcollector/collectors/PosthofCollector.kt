d99c045d1d70af501b6b454f52b44bebc884ca2f
abl <ableimat@gmx.at>
2024-10-05 13:58:03 +0200

    rewrote PosthofCollector

--- boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/PosthofCollector.kt
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/PosthofCollector.kt
@@ -3,77 +3,107 @@ package events.boudicca.eventcollector.collectors
 import base.boudicca.SemanticKeys
 import base.boudicca.api.eventcollector.Fetcher
 import base.boudicca.api.eventcollector.TwoStepEventCollector
-import base.boudicca.model.Event
+import base.boudicca.model.Registration
+import base.boudicca.model.structured.StructuredEvent
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+import java.net.URI
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 
-//TODO needs redo, they relaunched their site
-class PosthofCollector : TwoStepEventCollector<Element>("posthof") {
+class PosthofCollector : TwoStepEventCollector<String>("posthof") {
 
-    override fun getAllUnparsedEvents(): List<Element> {
-        val fetcher = Fetcher()
-        val events = mutableListOf<Element>()
+    private val fetcher = Fetcher()
+    private val baseUrl = "https://www.posthof.at"
 
-        val document = Jsoup.parse(fetcher.fetchUrl("https://www.posthof.at/programm/alles/browse/1/"))
-        val otherUrls = document.select("div.news-list-browse table a")
-            .toList()
-            .filter {
-                it.text() != "1"
+    override fun getAllUnparsedEvents(): List<String> {
+        val eventUrls = mutableListOf<String>()
+
+        var nextUrl: String = baseUrl
+        while (true) {
+            val currentDoc = Jsoup.parse(fetcher.fetchUrl(nextUrl))
+            eventUrls.addAll(currentDoc.select("ul.programmlist li h2 a").map { baseUrl + it.attr("href") })
+
+            val nextButton = currentDoc.select("ul.programmlist li.loadnext button")
+            if (nextButton.isNotEmpty()) {
+                nextUrl = baseUrl + nextButton.attr("hx-post")
+            } else {
+                break
             }
-            .map { it.attr("href") }
-        parseEventList(document, events)
-
-        otherUrls.forEach {
-            parseEventList(Jsoup.parse(fetcher.fetchUrl("https://www.posthof.at/$it")), events)
         }
 
-        return events
+        return eventUrls
     }
 
-    private fun parseEventList(document: Document, events: MutableList<Element>) {
-        events.addAll(document.select("div.event-list-item"))
-    }
+    override fun parseStructuredEvent(event: String): StructuredEvent {
+        val eventSite: Element = Jsoup.parse(fetcher.fetchUrl(event))
 
-    override fun parseEvent(event: Element): Event {
-        val data = mutableMapOf<String, String>()
+        var name = eventSite.select("div.tx-posthof-events h2 a").textNodes().first().text()
 
-        var name = event.select("div.h3>a").text()
-        name += " - ${event.select("div.news-list-subtitle").text()}"
+        val subtextSpan = eventSite.select("div.tx-posthof-events h2 a span")
+        if (subtextSpan.isNotEmpty()) {
+            name += " - " + subtextSpan.text()
+        }
+
+        val dateAndTypeSpans = getDateAndTypeSpans(eventSite)
+        val dateAndTimeText = getDateAndTimeText(dateAndTypeSpans)
         val startDate = LocalDateTime.parse(
-            event.select("span.news-list-date").text().substring(4),
-            DateTimeFormatter.ofPattern("dd.MM.uuuu // kk:mm")
-        ).atZone(ZoneId.of("Europe/Vienna"))
+            dateAndTimeText,
+            DateTimeFormatter.ofPattern("dd LLL uu kk:mm", Locale.GERMAN)
+        ).atZone(ZoneId.of("Europe/Vienna")).toOffsetDateTime()
 
-        mapType(data, event.select("span.news-list-category").text())
-        data[SemanticKeys.URL] = "https://www.posthof.at/" + event.select("div.h3>a").attr("href")
-        data[SemanticKeys.DESCRIPTION] = event.select("div.news_text>p").text()
-        data[SemanticKeys.PICTURE_URL] = "https://www.posthof.at/" + event.select("img").attr("src")
+        var description = ""
+        val textBlocks = eventSite.select("div.tx-posthof-events")[1].children().drop(1)
+        for (child in textBlocks) {
+            if (child.tagName() == "hr") {
+                break
+            }
+            description += child.text() + "\n"
+        }
 
-        data[SemanticKeys.REGISTRATION] = "ticket" //are there free events in posthof?
-        data[SemanticKeys.LOCATION_NAME] = "Posthof"
-        data[SemanticKeys.LOCATION_URL] = "https://www.posthof.at"
-        data[SemanticKeys.LOCATION_CITY] = "Linz"
-        data[SemanticKeys.SOURCES] = data[SemanticKeys.URL]!!
+        val imgUrl = baseUrl + eventSite.select("div.tx-posthof-events img").attr("src")
 
-        return Event(name, startDate.toOffsetDateTime(), data)
+        val builder = StructuredEvent.builder(name, startDate)
+        mapType(builder, dateAndTypeSpans[2].text())
+        return builder
+            .withProperty(SemanticKeys.DESCRIPTION_TEXT_PROPERTY, description)
+            .withProperty(SemanticKeys.PICTURE_URL_PROPERTY, URI.create(imgUrl))
+            .withProperty(SemanticKeys.REGISTRATION_PROPERTY, Registration.TICKET) //are there free events in posthof?
+            .withProperty(SemanticKeys.LOCATION_NAME_PROPERTY, "Posthof")
+            .withProperty(SemanticKeys.LOCATION_URL_PROPERTY, URI.create("https://www.posthof.at"))
+            .withProperty(SemanticKeys.LOCATION_CITY_PROPERTY, "Linz")
+            .withProperty(SemanticKeys.SOURCES_PROPERTY, listOf(event))
+            .withProperty(SemanticKeys.URL_PROPERTY, URI.create(event))
+            .build()
     }
 
-    private fun mapType(data: MutableMap<String, String>, type: String) {
+    private fun getDateAndTimeText(dateAndTypeSpans: Elements): String {
+        return (dateAndTypeSpans[0].text() + " " + dateAndTypeSpans[1].text())
+            .substring(4)
+            .replace("Jän", "Jan")
+    }
+
+    private fun getDateAndTypeSpans(eventSite: Element): Elements {
+        val preHeaders = eventSite.select("div.tx-posthof-events div.pre-header")
+        return preHeaders.last()!!.select("span")
+    }
+
+    //TODO move to enricher
+    private fun mapType(builder: StructuredEvent.StructuredEventBuilder, type: String) {
         val lowerType = type.lowercase()
         for (knownMusicType in KNOWN_MUSIC_TYPES) {
             if (lowerType.indexOf(knownMusicType) != -1) {
-                data[SemanticKeys.TYPE] = "concert"
-                data[SemanticKeys.CONCERT_GENRE] = type
+                builder.withProperty(SemanticKeys.TYPE_PROPERTY, "concert")
+                builder.withProperty(SemanticKeys.CONCERT_GENRE_PROPERTY, type)
                 return
             }
         }
         if (type.isNotBlank()) {
-            data[SemanticKeys.TYPE] = type
+            builder.withProperty(SemanticKeys.TYPE_PROPERTY, type)
         }
     }
 

b5a3f0d576c805f4fac8c9471e05e34d968c8270
abl <ableimat@gmx.at>
2024-11-29 18:57:11 +0100

    fix todos

--- boudicca.base/publisher-event-html/build.gradle.kts
+++ boudicca.base/publisher-event-html/build.gradle.kts
@@ -4,13 +4,8 @@ plugins {
 
 dependencies {
     api(project(":boudicca.base:search-client"))
-    api("com.github.jknack:handlebars:4.4.0")
-    testImplementation("com.microsoft.playwright:playwright:1.49.0")
-    testImplementation("com.deque.html.axe-core:playwright:4.10.1")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
-//TODO make this use version catalogue
-//    api(libs.handlebars)
-//    testImplementation(libs.mockito.kotlin)
-//    testImplementation(libs.microsoft.playwright)
-//    testImplementation(libs.axe.core.playwright)
+    api(libs.handlebars)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.microsoft.playwright)
+    testImplementation(libs.axe.core.playwright)
 }

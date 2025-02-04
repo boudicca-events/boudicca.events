494594125f7b46d8216317cb7bd99ef762bc1a6e
Matthias Holzinger <yolgie@cnoize.at>
2025-02-04 18:52:30 +0100

    fix: detekt warnings

--- boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/FamilienkarteEventCollector.kt
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/FamilienkarteEventCollector.kt
@@ -22,10 +22,11 @@ class FamilienkarteEventCollector : TwoStepEventCollector<String>("familienkarte
     private val baseUrl = "https://www.familienkarte.at"
 
     override fun getAllUnparsedEvents(): List<String> {
-        val dateFrom = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-        val dateTo = LocalDate.now().plusYears(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-        val eventsUrl =
-            "$baseUrl/de/freizeit/veranstaltungen/veranstaltungskalender.html?events_cat_key=8&date_from=$dateFrom&date_to=$dateTo"
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val dateFrom = LocalDate.now().format(dateTimeFormatter)
+        val dateTo = LocalDate.now().plusYears(1).format(dateTimeFormatter)
+        @Suppress("MaxLineLength") // this is an url that would not gain readability by linebreaking
+        val eventsUrl = "$baseUrl/de/freizeit/veranstaltungen/veranstaltungskalender.html?events_cat_key=8&date_from=$dateFrom&date_to=$dateTo"
         val document = Jsoup.parse(fetcher.fetchUrl(eventsUrl))
         return document
             .select("div.detailButton a")
@@ -37,7 +38,7 @@ class FamilienkarteEventCollector : TwoStepEventCollector<String>("familienkarte
         val eventSite = Jsoup.parse(fetcher.fetchUrl(eventUrl))
 
         val name = eventSite.select("div.eventDetailWrapper h1").text()
-        val (startDate, endDate) = parseDates(eventSite)
+        val (startDate, endDate) = parseStartAndEndDateTime(eventSite)
         val pictureUrl = eventSite.select("div.eventEntry img")
             .first()
             ?.attr("src")
@@ -56,29 +57,41 @@ class FamilienkarteEventCollector : TwoStepEventCollector<String>("familienkarte
     }
 
 
-    private fun parseDates(element: Element): Pair<OffsetDateTime, OffsetDateTime?> {
+    private fun parseStartAndEndDateTime(element: Element): Pair<OffsetDateTime, OffsetDateTime?> {
         val fullDateString = element.select("div.eventDetailWrapper").first()?.text()
             ?: throw IllegalArgumentException("Could not find element containing start date")
 
-        val dateRegex = """\b(\d{2})\.(\d{2})\.(\d{2})\b""".toRegex()
-        val dateMatch = dateRegex.find(fullDateString)
-            ?: throw IllegalArgumentException("Could not find date in $fullDateString")
-        val (day, month, year) = dateMatch.destructured
-        val formattedDate = "$day.$month.$year"
-        val localDate = LocalDate.parse(formattedDate, DateTimeFormatter.ofPattern("dd.MM.uu"))
-
-        val timeRegex = """\b(\d{2}:\d{2})(?:\s*-\s*(\d{2}:\d{2}))?\b""".toRegex()
-        val timeMatch = timeRegex.find(fullDateString)
-            ?: throw IllegalArgumentException("Could not find start (& endtime) in $fullDateString")
-        val (startTimeString, endTimeString) = timeMatch.destructured
-        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
-
-        val startTime = LocalTime.parse(startTimeString, timeFormatter)
-        val endTime = endTimeString.takeIf { it.isNotEmpty() }?.let { LocalTime.parse(it, timeFormatter) }
+        val localDate = parseDate(fullDateString)
+        val (startTime, endTime) = parseStartAndEndtime(fullDateString)
 
         return Pair(
             localDate.atTime(startTime).atZone(ZoneId.of("Europe/Vienna")).toOffsetDateTime(),
             localDate.atTime(endTime).atZone(ZoneId.of("Europe/Vienna")).toOffsetDateTime(),
         )
+    }
+
+    private fun parseDate(fullDateString: String): LocalDate {
+        val dateRegex = """\b(\d{2})\.(\d{2})\.(\d{2})\b""".toRegex()
+        val dateMatch = dateRegex.find(fullDateString)
+            ?: throw IllegalArgumentException("Could not find date in $fullDateString")
+        val (day, month, year) = dateMatch.destructured
+
+        val formattedDate = "$day.$month.$year"
+        val localDate = LocalDate.parse(formattedDate, DateTimeFormatter.ofPattern("dd.MM.uu"))
+
+        return localDate
+    }
+
+    private fun parseStartAndEndtime(fullDateString: String): Pair<LocalTime?, LocalTime?> {
+        val timeRegex = """\b(\d{2}:\d{2})(?:\s*-\s*(\d{2}:\d{2}))?\b""".toRegex()
+        val timeMatch = timeRegex.find(fullDateString)
+            ?: throw IllegalArgumentException("Could not find start (& endtime) in $fullDateString")
+        val (startTimeString, endTimeString) = timeMatch.destructured
+
+        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+        val startTime = LocalTime.parse(startTimeString, timeFormatter)
+        val endTime = endTimeString.takeIf { it.isNotEmpty() }?.let { LocalTime.parse(it, timeFormatter) }
+
+        return Pair(startTime, endTime)
     }
 }

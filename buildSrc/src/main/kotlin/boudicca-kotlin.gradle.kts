69126b43120617d65dbefa9512d4ebdf0a8e08b7
abl <ableimat@gmx.at>
2025-02-26 00:16:12 +0100

    fix slf4j dependency

--- buildSrc/src/main/kotlin/boudicca-kotlin.gradle.kts
+++ buildSrc/src/main/kotlin/boudicca-kotlin.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
 
 val versionCatalog = versionCatalogs.named("libs")
 dependencies {
+    implementation(versionCatalog.findLibrary("slf4j").get())
     implementation(versionCatalog.findLibrary("kotlin-logging").get())
     testImplementation(platform(versionCatalog.findLibrary("junit-jupiter-bom").get()))
     testImplementation(versionCatalog.findLibrary("junit-jupiter").get())

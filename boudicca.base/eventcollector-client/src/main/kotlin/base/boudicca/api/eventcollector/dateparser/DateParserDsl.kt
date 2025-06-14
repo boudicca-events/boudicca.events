4b890a440def0780cb012c97b87994d1ec1c4bc9
abl <ableimat@gmx.at>
2025-06-14 16:53:38 +0200

    SchlachthofCollector

--- boudicca.base/eventcollector-client/src/main/kotlin/base/boudicca/api/eventcollector/dateparser/DateParserDsl.kt
+++ boudicca.base/eventcollector-client/src/main/kotlin/base/boudicca/api/eventcollector/dateparser/DateParserDsl.kt
@@ -1,5 +1,8 @@
 package base.boudicca.api.eventcollector.dateparser
 
+import base.boudicca.SemanticKeys
+import base.boudicca.model.structured.StructuredEvent
+import base.boudicca.model.structured.dsl.StructuredEventBuilder
 import java.time.LocalDate
 import java.time.LocalTime
 import java.time.OffsetDateTime
@@ -34,4 +37,17 @@ fun localTimeParser(init: DateParser.() -> Unit): LocalTime {
     dateParser.init()
 
     return dateParser.parseLocalTime()
+}
+
+fun structuredEvent(
+    name: String,
+    dates: DateParserResult,
+    init: StructuredEventBuilder.() -> Unit = {}
+): List<StructuredEvent> {
+    return dates.dates.map {
+        val builder = StructuredEventBuilder(name, it.startDate)
+        builder.withProperty(SemanticKeys.ENDDATE_PROPERTY, it.endDate)
+        builder.init()
+        builder.build()
+    }
 }

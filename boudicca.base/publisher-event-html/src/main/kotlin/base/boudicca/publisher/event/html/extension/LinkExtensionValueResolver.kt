68a8a70516bc8e858ace14befc1e1c79dd7b401f
Bernadette Hammerle <bernadette.elena.hammerle@gmail.com>
2025-07-25 20:53:41 +0200

    fetch footer links to remove them from base

--- boudicca.base/publisher-event-html/src/main/kotlin/base/boudicca/publisher/event/html/extension/LinkExtensionValueResolver.kt
+++ boudicca.base/publisher-event-html/src/main/kotlin/base/boudicca/publisher/event/html/extension/LinkExtensionValueResolver.kt
@@ -1,17 +1,19 @@
 package base.boudicca.publisher.event.html.extension
 
 import com.github.jknack.handlebars.ValueResolver
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-class HeaderExtensionValueResolver(extensions: List<Extension>) : ValueResolver {
+class LinkExtensionValueResolver(extensions: List<Extension>) : ValueResolver {
 
     private val headers: List<Map<String, String>> = getAllExtensionHeaders(extensions)
+    private val footers: List<Map<String, String>> = getAllExtensionFooters(extensions)
 
     override fun resolve(context: Any?, name: String?): Any {
         return if (name == "extensionHeaders") {
             headers
+        } else if (name == "extensionFooters") {
+            footers
         } else {
             ValueResolver.UNRESOLVED
         }
@@ -27,6 +29,17 @@ class HeaderExtensionValueResolver(extensions: List<Extension>) : ValueResolver 
 
     private fun getAllExtensionHeaders(extensions: List<Extension>): List<Map<String, String>> {
         return extensions.flatMap { extension -> extension.getHeaders() }
+            .map { header ->
+                mapOf(
+                    "text" to header.text,
+                    "url" to header.url,
+                    "target" to header.target,
+                )
+            }
+    }
+
+    private fun getAllExtensionFooters(extensions: List<Extension>): List<Map<String, String>> {
+        return extensions.flatMap { extension -> extension.getFooters() }
             .map { header ->
                 mapOf(
                     "text" to header.text,

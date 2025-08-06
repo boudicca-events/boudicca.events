f94d192319deade2462c9728c83ffb2b9b91f32d
abl <ableimat@gmx.at>
2025-08-06 21:34:49 +0200

    fix log capturing in eventcollectors

--- boudicca.base/eventcollector-client/src/main/kotlin/base/boudicca/api/eventcollector/webui/NoopLoggingSystemFactory.kt
+++ boudicca.base/eventcollector-client/src/main/kotlin/base/boudicca/api/eventcollector/webui/NoopLoggingSystemFactory.kt
@@ -2,12 +2,13 @@ package base.boudicca.api.eventcollector.webui
 
 import org.springframework.boot.logging.LoggingSystem
 import org.springframework.boot.logging.LoggingSystemFactory
+import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 
 /**
  * we want to disable springs autoconfiguration of logging since we do this ourselves
  */
-@Order(0)
+@Order(Ordered.HIGHEST_PRECEDENCE)
 class NoopLoggingSystemFactory : LoggingSystemFactory {
     override fun getLoggingSystem(classLoader: ClassLoader?): LoggingSystem {
         return object : LoggingSystem() {

da2d44b99e26b109e16acf1b49b333574decb6d8
abl <ableimat@gmx.at>
2025-08-22 10:07:29 +0200

    add support for webp

--- boudicca.base/publisher-event-html/build.gradle.kts
+++ boudicca.base/publisher-event-html/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     api(libs.spring.boot.starter.web)
     api(libs.handlebars)
     api(libs.handlebars.springmvc)
+    implementation(libs.bundles.twelvemonkeys)
     implementation(libs.otel.java.httpclient)
     testImplementation(libs.microsoft.playwright)
     testImplementation(libs.axe.core.playwright)

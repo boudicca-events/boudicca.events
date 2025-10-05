1ba3b5d999c32a187265d7f36975418a60341a4d
abl <ableimat@gmx.at>
2025-10-05 13:05:18 +0200

    add english months

--- boudicca.base/dateparser-lib/src/main/kotlin/base/boudicca/dateparser/dateparser/impl/MonthMappings.kt
+++ boudicca.base/dateparser-lib/src/main/kotlin/base/boudicca/dateparser/dateparser/impl/MonthMappings.kt
@@ -6,17 +6,25 @@ internal object MonthMappings {
     private val MONTH_MAPPINGS = mapOf(
         "januar" to 1,
         "jänner" to 1,
+        "january" to 1,
         "februar" to 2,
+        "february" to 2,
         "märz" to 3,
+        "march" to 3,
         "april" to 4,
         "mai" to 5,
+        "may" to 5,
         "juni" to 6,
+        "june" to 6,
         "juli" to 7,
+        "july" to 7,
         "august" to 8,
         "september" to 9,
         "oktober" to 10,
+        "october" to 10,
         "november" to 11,
         "dezember" to 12,
+        "december" to 12,
     )
 
     internal fun mapMonthToInt(month: String?): Int? {

cc4f050512b78a8c9ae8f81c1927e70d1ef8e74f
Matthias 'Yolgie' Holzinger <yolgie@cnoize.at>
2025-10-20 17:35:22 +0200

    feature(#817): add a publisher endpoint to get a single entry by id from the db
    Co-authored-by: Matthias Ableidinger <ableimat@gmx.at>

--- boudicca.base/entrydb/src/main/kotlin/base/boudicca/entrydb/controller/PublisherController.kt
+++ boudicca.base/entrydb/src/main/kotlin/base/boudicca/entrydb/controller/PublisherController.kt
@@ -4,21 +4,24 @@ import base.boudicca.api.eventdb.PublisherApi
 import base.boudicca.entrydb.service.EntryService
 import base.boudicca.model.Entry
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
-@RequestMapping("/entries")
+@RequestMapping()
 class PublisherController @Autowired constructor(
     private val entryService: EntryService,
 ) : PublisherApi {
 
-    @GetMapping("")
+    @GetMapping("/entries")
     @ResponseBody
     override fun all(): Set<Entry> {
         return entryService.all()
     }
 
+    @GetMapping("/entry/{boudiccaId}")
+    @ResponseBody
+    override fun entry(@PathVariable boudiccaId: UUID): Entry? {
+        return entryService.get(boudiccaId)
+    }
 }

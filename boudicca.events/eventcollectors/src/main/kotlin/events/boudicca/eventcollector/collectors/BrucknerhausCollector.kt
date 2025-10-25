04e695621478a337e1f7154c0d2fd1e4e2324363
Bernadette Hammerle <bernadette.elena.hammerle@gmail.com>
2025-10-25 14:53:50 +0200

    brucknerhaus: add img alt and copyright; add other locations

--- boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/BrucknerhausCollector.kt
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/BrucknerhausCollector.kt
@@ -2,10 +2,10 @@ package events.boudicca.eventcollector.collectors
 
 import base.boudicca.SemanticKeys
 import base.boudicca.api.eventcollector.TwoStepEventCollector
+import base.boudicca.api.eventcollector.util.FetcherFactory
+import base.boudicca.api.eventcollector.util.structuredEvent
 import base.boudicca.dateparser.dateparser.DateParser
 import base.boudicca.dateparser.dateparser.DateParserResult
-import base.boudicca.api.eventcollector.util.structuredEvent
-import base.boudicca.api.eventcollector.util.FetcherFactory
 import base.boudicca.format.UrlUtils
 import base.boudicca.model.structured.StructuredEvent
 import org.jsoup.Jsoup
@@ -14,16 +14,17 @@ import org.jsoup.nodes.Element
 
 
 class BrucknerhausCollector : TwoStepEventCollector<Element>("brucknerhaus") {
+    private val baseUrl = "https://www.brucknerhaus.at"
 
     override fun getAllUnparsedEvents(): List<Element> {
         val events = mutableListOf<Element>()
 
         val fetcher = FetcherFactory.newFetcher()
-        val document = Jsoup.parse(fetcher.fetchUrl("https://www.brucknerhaus.at/programm/veranstaltungen"))
+        val document = Jsoup.parse(fetcher.fetchUrl("$baseUrl/programm/veranstaltungen"))
         val otherUrls = document.select("ul.pagination>li")
             .toList()
             .filter { it.attr("class").isEmpty() }
-            .map { "https://www.brucknerhaus.at" + it.children().first()!!.attr("href") }
+            .map { baseUrl + it.children().first()!!.attr("href") }
 
         events.addAll(findUnparsedEvents(document))
 
@@ -41,24 +42,38 @@ class BrucknerhausCollector : TwoStepEventCollector<Element>("brucknerhaus") {
     override fun parseMultipleStructuredEvents(event: Element): List<StructuredEvent> {
         val startDates = parseDate(event)
         val name = event.select("div.event__name").text()
-        val url = "https://www.brucknerhaus.at" + event.select("a.headline_link").attr("href")
+        val url = baseUrl + event.select("a.headline_link").attr("href")
 
         var description = event.select("div.event__teaser p").text()
         if (description.isBlank()) {
             description = event.select("div.event__teaser .fr-view").first()?.children()?.first()?.text() ?: ""
         }
 
+        val imgTag = event.select("div.event__image img")
+        val imgAltText = imgTag.attr("alt")
+        var imgCopyright = "Brucknerhaus Linz"
+        if (imgAltText.contains("©")) {
+            imgCopyright = imgAltText.split("©")[1].trim()
+        }
+
+        var location = event.select(".event__location").text()
+        location = if (location.contains("Brucknerhaus")) {
+            "Brucknerhaus Linz"
+        } else {
+            // clean up location that looks like "18:00 Haupteingang Anton Bruckner Privatuniversität Linz"
+            location.substring(5).replace("Haupteingang", "").trim()
+        }
+
         return structuredEvent(name, startDates) {
             withProperty(SemanticKeys.URL_PROPERTY, UrlUtils.parse(url))
             withProperty(SemanticKeys.SOURCES_PROPERTY, listOf(url))
-            withProperty(
-                SemanticKeys.PICTURE_URL_PROPERTY,
-                UrlUtils.parse(event.select("div.event__image img").attr("src"))
-            )
+            withProperty(SemanticKeys.PICTURE_URL_PROPERTY, UrlUtils.parse(imgTag.attr("src")))
+            withProperty(SemanticKeys.PICTURE_ALT_TEXT_PROPERTY, imgAltText)
+            withProperty(SemanticKeys.PICTURE_COPYRIGHT_PROPERTY, imgCopyright)
             withProperty(SemanticKeys.DESCRIPTION_TEXT_PROPERTY, description)
             withProperty(SemanticKeys.TYPE_PROPERTY, "concert") //TODO check
-            withProperty(SemanticKeys.LOCATION_NAME_PROPERTY, "Brucknerhaus") //TODO not all events are there...
-            withProperty(SemanticKeys.LOCATION_URL_PROPERTY, UrlUtils.parse("https://www.brucknerhaus.at/"))
+            withProperty(SemanticKeys.LOCATION_NAME_PROPERTY, location)
+            withProperty(SemanticKeys.LOCATION_URL_PROPERTY, UrlUtils.parse(baseUrl))
             withProperty(SemanticKeys.LOCATION_CITY_PROPERTY, "Linz")
         }
     }

f36d201f7284b856f5b96f43d26279e9a2d20e97
Bernadette Hammerle <bernadette.elena.hammerle@gmail.com>
2025-10-26 20:04:52 +0100

    spinnerei: add img copyright

--- boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/SpinnereiCollector.kt
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/SpinnereiCollector.kt
@@ -2,23 +2,23 @@ package events.boudicca.eventcollector.collectors
 
 import base.boudicca.SemanticKeys
 import base.boudicca.api.eventcollector.TwoStepEventCollector
+import base.boudicca.api.eventcollector.util.FetcherFactory
+import base.boudicca.api.eventcollector.util.structuredEvent
 import base.boudicca.dateparser.dateparser.DateParser
 import base.boudicca.dateparser.dateparser.DateParserResult
-import base.boudicca.api.eventcollector.util.structuredEvent
-import base.boudicca.api.eventcollector.util.FetcherFactory
 import base.boudicca.format.UrlUtils
 import base.boudicca.model.structured.StructuredEvent
 import org.jsoup.Jsoup
 
 class SpinnereiCollector : TwoStepEventCollector<String>("spinnerei") {
-
+    private val baseUrl = "https://spinnerei.kulturpark.at"
     val fetcher = FetcherFactory.newFetcher()
 
     override fun getAllUnparsedEvents(): List<String> {
         val events = mutableListOf<String>()
         val eventUrls = mutableSetOf<String>()
 
-        val document = Jsoup.parse(fetcher.fetchUrl("https://spinnerei.kulturpark.at/programm/"))
+        val document = Jsoup.parse(fetcher.fetchUrl("$baseUrl/programm/"))
         document.select("div.container-programm-uebersicht > a")
             .forEach { eventUrls.add(it.attr("href")) }
 
@@ -38,9 +38,7 @@ class SpinnereiCollector : TwoStepEventCollector<String>("spinnerei") {
         val (startDate, type) = parseTypeAndDate(doc.select("div.vng-details div.vng-detail-content-beginn").text())
         val picture = doc.select("div.vng-details div.bg-image").first()
         val pictureUrl = if (picture != null) {
-            "https://spinnerei.kulturpark.at" + parsePictureUrl(
-                picture.attr("style")
-            )
+            baseUrl + parsePictureUrl(picture.attr("style"))
         } else {
             null
         }
@@ -50,15 +48,16 @@ class SpinnereiCollector : TwoStepEventCollector<String>("spinnerei") {
             withProperty(SemanticKeys.DESCRIPTION_TEXT_PROPERTY, doc.select("div.vng-detail-content-bodytext").text())
             withProperty(SemanticKeys.TYPE_PROPERTY, type)
             withProperty(SemanticKeys.PICTURE_URL_PROPERTY, UrlUtils.parse(pictureUrl))
+            withProperty(SemanticKeys.PICTURE_COPYRIGHT_PROPERTY, "Spinnerei")
             withProperty(SemanticKeys.LOCATION_NAME_PROPERTY, "Spinnerei")
-            withProperty(SemanticKeys.LOCATION_URL_PROPERTY, UrlUtils.parse("https://spinnerei.kulturpark.at/"))
+            withProperty(SemanticKeys.LOCATION_URL_PROPERTY, UrlUtils.parse(baseUrl))
             withProperty(SemanticKeys.LOCATION_CITY_PROPERTY, "Traun")
             withProperty(SemanticKeys.SOURCES_PROPERTY, listOf(event))
         }
     }
 
     private fun parsePictureUrl(style: String): String {
-        //looks like background-image:url('/media/47704/c-wolf-gruber_hypnotica_pr-foto.png?center=0.39316239316239315,0.22666666666666666&mode=crop&width=320&height=180');
+        // looks like background-image:url('/media/47704/c-wolf-gruber_hypnotica_pr-foto.png?center=0.39316239316239315,0.22666666666666666&mode=crop&width=320&height=180');
         return style.substring(22, style.length - 3)
     }
 
@@ -68,5 +67,4 @@ class SpinnereiCollector : TwoStepEventCollector<String>("spinnerei") {
 
         return Pair(DateParser.parse(text), split[3])
     }
-
 }

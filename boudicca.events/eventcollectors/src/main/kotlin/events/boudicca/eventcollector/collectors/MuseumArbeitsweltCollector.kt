3f965d52ff6fdcce2b7da0a75c0ce8464692c32c
Bernadette Hammerle <bernadette.elena.hammerle@gmail.com>
2025-10-31 13:33:28 +0100

    museumarbeitswelt: add img copyright, simplify img url

--- boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/MuseumArbeitsweltCollector.kt
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/MuseumArbeitsweltCollector.kt
@@ -16,7 +16,7 @@ class MuseumArbeitsweltCollector : TwoStepEventCollector<Pair<String, String>>("
     override fun getAllUnparsedEvents(): List<Pair<String, String>> {
         val events = mutableListOf<Pair<String, String>>()
         val document = Jsoup.parse(fetcher.fetchUrl("https://museumarbeitswelt.at/kalender/"))
-        document.select("div.ecs-event").forEach() {
+        document.select("div.ecs-event").forEach {
             events.add(
                 Pair(
                     it.select("a.act-view-more").attr("href"),
@@ -37,20 +37,14 @@ class MuseumArbeitsweltCollector : TwoStepEventCollector<Pair<String, String>>("
         val description = eventSite.select("div.et_pb_post_content").text()
 
         val img = eventSite.select("div.et_pb_title_featured_container span.et_pb_image_wrap img")
-        val pictureUrl = if (!img.isEmpty()) {
-            img.last()!!.attr("src")
-        } else {
-            null
-        }
+        val pictureUrl = img.last()?.attr("src")
 
         return structuredEvent(name, startDate) {
             withProperty(SemanticKeys.URL_PROPERTY, UrlUtils.parse(eventUrl))
             withProperty(SemanticKeys.SOURCES_PROPERTY, listOf(eventUrl))
             withProperty(SemanticKeys.DESCRIPTION_TEXT_PROPERTY, description)
-            withProperty(
-                SemanticKeys.PICTURE_URL_PROPERTY,
-                if (pictureUrl != null) UrlUtils.parse(pictureUrl) else null
-            )
+            withProperty(SemanticKeys.PICTURE_URL_PROPERTY, UrlUtils.parse(pictureUrl))
+            withProperty(SemanticKeys.PICTURE_COPYRIGHT_PROPERTY, "Museum Arbeitswelt")
             withProperty(SemanticKeys.LOCATION_NAME_PROPERTY, "Museum Arbeitswelt")
             withProperty(SemanticKeys.LOCATION_URL_PROPERTY, UrlUtils.parse("https://museumarbeitswelt.at/"))
             withProperty(SemanticKeys.LOCATION_CITY_PROPERTY, "Steyr")
@@ -60,5 +54,4 @@ class MuseumArbeitsweltCollector : TwoStepEventCollector<Pair<String, String>>("
     private fun parseDate(dateToParse: String): DateParserResult {
         return DateParser.parse(dateToParse)
     }
-
 }

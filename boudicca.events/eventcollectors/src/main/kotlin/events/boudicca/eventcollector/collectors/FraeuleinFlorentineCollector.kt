5999f9e7f85ca6c1e4d0ac15671c5ce422034829
Bernadette Hammerle <bernadette.elena.hammerle@gmail.com>
2025-11-01 21:21:34 +0100

    florentine: add logo and copyright, remove space in name

--- boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/FraeuleinFlorentineCollector.kt
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/FraeuleinFlorentineCollector.kt
@@ -13,17 +13,18 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
-import java.util.*
-
+import java.util.Locale
 
 class FraeuleinFlorentineCollector : TwoStepEventCollector<Element>("fraeuleinflorentine") {
-
     private val fetcher = FetcherFactory.newFetcher()
     private val baseUrl = "https://frl-florentine.at/eventkalender/"
 
     override fun getAllUnparsedEvents(): List<Element> {
-        return Jsoup.parse(fetcher.fetchUrl(baseUrl))
+        val eventSite = Jsoup.parse(fetcher.fetchUrl(baseUrl))
+        val logo = eventSite.select(".page-content img")
+        return eventSite
             .select("ul.simcal-events li")
+            .append(logo.toString()) // add logo of header to each event
             .distinctBy { it.text() } // multi-day events have the same text in multiple entries
     }
 
@@ -36,14 +37,17 @@ class FraeuleinFlorentineCollector : TwoStepEventCollector<Element>("fraeuleinfl
         if (endDate != null) {
             startDate = DateParserResult(listOf(DatePair(startDate.dates[0].startDate, endDate.dates[0].startDate)))
         }
+        val pictureUrl = event.select("img").attr("src")
 
         return structuredEvent(name, startDate) {
             withProperty(SemanticKeys.URL_PROPERTY, UrlUtils.parse(baseUrl))
             withProperty(SemanticKeys.SOURCES_PROPERTY, listOf(baseUrl))
             withProperty(SemanticKeys.DESCRIPTION_TEXT_PROPERTY, description)
-            withProperty(SemanticKeys.LOCATION_NAME_PROPERTY, "Salonschiff Fräulein Florentine ")
+            withProperty(SemanticKeys.LOCATION_NAME_PROPERTY, "Salonschiff Fräulein Florentine")
             withProperty(SemanticKeys.LOCATION_URL_PROPERTY, UrlUtils.parse("https://frl-florentine.at"))
             withProperty(SemanticKeys.LOCATION_CITY_PROPERTY, "Linz")
+            withProperty(SemanticKeys.PICTURE_URL_PROPERTY, UrlUtils.parse(pictureUrl))
+            withProperty(SemanticKeys.PICTURE_COPYRIGHT_PROPERTY, "Salonschiff Fräulein Florentine")
         }
     }
 
@@ -67,12 +71,13 @@ class FraeuleinFlorentineCollector : TwoStepEventCollector<Element>("fraeuleinfl
     }
 
     private fun parseEndDate(event: Element, startDate: DateParserResult): DateParserResult? {
-        var endTime : LocalTime? = null
+        var endTime: LocalTime? = null
         val endTimeToParse = event.select(".simcal-event-end-time").text()
         if (endTimeToParse.isNotBlank()) {
             endTime = LocalTime.parse( // DateParse can't handle am/pm, so convert it manually and continue with its String
                 endTimeToParse.uppercase(), // convert pm to PM, to be recognized by the time pattern 'a'
-                DateTimeFormatter.ofPattern("h:mm a").withLocale(Locale.GERMAN))
+                DateTimeFormatter.ofPattern("h:mm a").withLocale(Locale.GERMAN)
+            )
         }
 
         var endDate: DateParserResult? = null
@@ -80,12 +85,10 @@ class FraeuleinFlorentineCollector : TwoStepEventCollector<Element>("fraeuleinfl
         if (endDateToParse.isNotBlank()) { // endDate with endDate or end of day if not time is set
             val time = endTime ?: LocalTime.MAX
             endDate = DateParser.parse(endDateToParse, time.toString())
-
         } else if (endTime != null) { // startDate with endTime
             endDate = DateParser.parse(startDate.dates[0].startDate.toLocalDate().toString(), endTime.toString())
         }
 
         return endDate
     }
-
 }

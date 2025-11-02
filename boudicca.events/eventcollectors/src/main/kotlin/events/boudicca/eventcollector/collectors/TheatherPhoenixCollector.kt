279304a1d110210e678c335a33f23d819d0dd222
Bernadette Hammerle <bernadette.elena.hammerle@gmail.com>
2025-11-02 11:15:15 +0100

    theaterphoenix: add picture copyright

--- boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/TheatherPhoenixCollector.kt
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/TheatherPhoenixCollector.kt
@@ -11,11 +11,11 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 class TheatherPhoenixCollector : TwoStepEventCollector<Pair<Element, Element>>("theaterphoenix") {
-
+    private val baseUrl = "https://www.theater-phoenix.at"
     private val fetcher = FetcherFactory.newFetcher()
 
     override fun getAllUnparsedEvents(): List<Pair<Element, Element>> {
-        val document = Jsoup.parse(fetcher.fetchUrl("https://www.theater-phoenix.at/termine"))
+        val document = Jsoup.parse(fetcher.fetchUrl("$baseUrl/termine"))
         return document.select("div.terminecol div.onecolum > div.terminlink")
             .map {
                 val slug = it.attr("rel").removePrefix("/termine/")
@@ -27,30 +27,31 @@ class TheatherPhoenixCollector : TwoStepEventCollector<Pair<Element, Element>>("
     override fun parseMultipleStructuredEvents(event: Pair<Element, Element>): List<StructuredEvent?> {
         val (link, details) = event
 
-        var name = link.select("div.terminetitel").text()
+        val name = link.select("div.terminetitel").text()
 
         val description = details.select("div.termindetailbeschreibung p").text()
 
         val date = DateParser.parse(details.select("div.termindetaildatumzeit").text())
 
         val picture = details.select("div.termindetailbeschreibung img")
-        val pictureUrl = "https://www.theater-phoenix.at" + picture.attr("src")
+        val pictureUrl = baseUrl + picture.attr("src")
         val pictureAlt = picture.attr("alt").trim()
+        val pictureCopyright = picture.attr("title").trim().ifBlank { "Theater Phönix" }
 
         return structuredEvent(name, date) {
             withProperty(
                 SemanticKeys.URL_PROPERTY,
-                UrlUtils.parse("https://www.theater-phoenix.at" + link.attr("rel"))
+                UrlUtils.parse(baseUrl + link.attr("rel"))
             )
             withProperty(SemanticKeys.TYPE_PROPERTY, "theater")
             withProperty(SemanticKeys.PICTURE_URL_PROPERTY, UrlUtils.parse(pictureUrl))
             withProperty(SemanticKeys.PICTURE_ALT_TEXT_PROPERTY, pictureAlt)
+            withProperty(SemanticKeys.PICTURE_COPYRIGHT_PROPERTY, pictureCopyright)
             withProperty(SemanticKeys.DESCRIPTION_TEXT_PROPERTY, description)
             withProperty(SemanticKeys.LOCATION_NAME_PROPERTY, "Theater Phönix")
-            withProperty(SemanticKeys.LOCATION_URL_PROPERTY, UrlUtils.parse("https://www.theater-phoenix.at"))
+            withProperty(SemanticKeys.LOCATION_URL_PROPERTY, UrlUtils.parse(baseUrl))
             withProperty(SemanticKeys.LOCATION_CITY_PROPERTY, "Linz")
-            withProperty(SemanticKeys.SOURCES_PROPERTY, listOf("https://www.theater-phoenix.at/termine"))
+            withProperty(SemanticKeys.SOURCES_PROPERTY, listOf("$baseUrl/termine"))
         }
     }
-
 }

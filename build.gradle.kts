882e6fd3b67ea142b8c5522a1b0ecc647ed2ea18
Matthias 'Yolgie' Holzinger <yolgie@cnoize.at>
2025-12-08 15:01:04 +0100

    build: fix JVM toolchain configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,2 @@
 version = file("version.txt").readText()
-ext["jvmVersion"] = 21
+ext["jvmVersion"] = 21 // be careful to keep this in sync with the buildSrc/build.gradle.kts
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,3 +10,7 @@ repositories {
 dependencies {
     implementation(libs.bundles.boudicca.plugins)
 }
+
+kotlin {
+    jvmToolchain(21) // be careful to keep this in sync with the global build.gradle.kts
+}

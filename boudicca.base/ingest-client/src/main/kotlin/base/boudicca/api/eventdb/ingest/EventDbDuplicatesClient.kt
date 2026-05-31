ea77f81a51551499b589902630054c33c16632f0
abl <ableimat@gmx.at>
2026-05-31 21:56:10 +0200

    event deduplication api definition

--- boudicca.base/ingest-client/src/main/kotlin/base/boudicca/api/eventdb/ingest/EventDbDuplicatesClient.kt
+++ boudicca.base/ingest-client/src/main/kotlin/base/boudicca/api/eventdb/ingest/EventDbDuplicatesClient.kt
@@ -1,6 +1,8 @@
 package base.boudicca.api.eventdb.ingest
 
 import base.boudicca.eventdb.openapi.api.DuplicatesApi
+import base.boudicca.model.Entry
+import base.boudicca.model.Event
 import base.boudicca.openapi.ApiClient
 import base.boudicca.openapi.ApiException
 import io.opentelemetry.api.GlobalOpenTelemetry

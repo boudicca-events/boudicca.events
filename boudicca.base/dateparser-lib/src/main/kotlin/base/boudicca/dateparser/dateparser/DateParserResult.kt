ffcc175b1a225ffca3c1eac7dfe7943490794528
abl <ableimat@gmx.at>
2026-07-26 11:53:35 +0200

    fix error in DateParserResult.reduce for empty collections

--- boudicca.base/dateparser-lib/src/main/kotlin/base/boudicca/dateparser/dateparser/DateParserResult.kt
+++ boudicca.base/dateparser-lib/src/main/kotlin/base/boudicca/dateparser/dateparser/DateParserResult.kt
@@ -10,7 +10,7 @@ data class DateParserResult(
     operator fun plus(other: DateParserResult): DateParserResult = DateParserResult(dates + other.dates)
 }
 
-fun Iterable<DateParserResult>.reduce(): DateParserResult = this.reduce { r1, r2 -> r1 + r2 }
+fun Collection<DateParserResult>.reduce(): DateParserResult = if (this.isNotEmpty()) this.reduce { r1, r2 -> r1 + r2 } else DateParserResult(emptyList())
 
 data class DatePair(
     val startDate: OffsetDateTime,

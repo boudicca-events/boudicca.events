dd3d63a3ad601a9abc5b223f74c454cce704d1c5
abl <ableimat@gmx.at>
2026-07-26 11:43:45 +0200

    fix TheaterInDerInnenstadtCollector

--- boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/TheaterInDerInnenstadtCollector.kt
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/TheaterInDerInnenstadtCollector.kt
@@ -11,43 +11,37 @@ import base.boudicca.model.Registration
 import base.boudicca.model.structured.StructuredEvent
 import events.boudicca.eventcollector.util.fetchUrlAndParse
 import events.boudicca.eventcollector.util.withDescription
-import org.jsoup.nodes.Element
 
-class TheaterInDerInnenstadtCollector : TwoStepEventCollector<Pair<Element, String?>>("theaterinderinnenstadt") {
+class TheaterInDerInnenstadtCollector : TwoStepEventCollector<String>("theaterinderinnenstadt") {
     private val fetcher = FetcherFactory.newFetcher()
     private val baseUrl = "https://theater-innenstadt.at/"
     private val eventUrl = baseUrl + "spielplan/"
 
-    override fun getAllUnparsedEvents(): List<Pair<Element, String?>> {
+    override fun getAllUnparsedEvents(): List<String> {
         val document = fetcher.fetchUrlAndParse(eventUrl)
-        val events = document.select("div.event")
-        val logoSrc = document.selectFirst("a.tm-logo img")?.attr("src")
-        return events.map { Pair(it, logoSrc) }
+        val eventLinks = document.select("#main-content div.flex.flex-col > a").map { it.attr("href") }
+        return eventLinks
+            // they have foreign urls we cannot parse, so only accept relative ones
+            .filter { !it.startsWith("http") }
     }
 
-    override fun parseMultipleStructuredEvents(event: Pair<Element, String?>): List<StructuredEvent?> {
-        val (eventSite, logoSrc) = event
-        val name = eventSite.select("span.evcal_event_title").text()
-        val description = eventSite.select("[itemprop=description]")
+    override fun parseMultipleStructuredEvents(event: String): List<StructuredEvent?> {
+        val eventSite = fetcher.fetchUrlAndParse(baseUrl + event.removePrefix("/"))
+        val title = eventSite.title()
+        val lastTitleDash = title.lastIndexOf('–')
+        val name = title.substring(0, lastTitleDash).trim()
+        val dateTimeString = title.substring(lastTitleDash).trim()
+        val startDateTime = DateParser.parse(dateTimeString)
 
-        val dateInfos = eventSite.select("span.evoet_dayblock")
-        val date = dateInfos.select(".date").text()
-        val month = dateInfos.select(".month").text()
-        val year = dateInfos.attr("data-syr")
-        val startDate = "$date.$month $year"
-        val startTime = dateInfos.select(".time").text()
-        val startDateTime = DateParser.parse(startDate, startTime)
+        val description = eventSite.select("div.prose")
 
-        var imgSrc = eventSite.select(".evocard_main_image img").attr("src")
-        if (imgSrc.isBlank() && !logoSrc.isNullOrBlank()) {
-            imgSrc = logoSrc
-        }
+        val imgSrc = eventSite.select("aside img").attr("src")
 
         val type: String? =
             when {
-                description.text().lowercase().contains("theater") -> "theater"
                 description.text().lowercase().contains("musical") -> "musical"
                 description.text().lowercase().contains("comedy") -> "comedy"
+                description.text().lowercase().contains("theater") -> "theater"
                 else -> null
             }
 
@@ -57,7 +51,7 @@ class TheaterInDerInnenstadtCollector : TwoStepEventCollector<Pair<Element, Stri
             withDescription(description)
             withProperty(SemanticKeys.CATEGORY_PROPERTY, EventCategory.ART)
             withProperty(SemanticKeys.TYPE_PROPERTY, type)
-            withProperty(SemanticKeys.PICTURE_URL_PROPERTY, UrlUtils.parse(imgSrc))
+            withProperty(SemanticKeys.PICTURE_URL_PROPERTY, UrlUtils.parse(baseUrl, imgSrc))
             withProperty(SemanticKeys.PICTURE_COPYRIGHT_PROPERTY, "Theater in der Innenstadt")
             withProperty(SemanticKeys.LOCATION_CITY_PROPERTY, "Linz")
             withProperty(SemanticKeys.LOCATION_NAME_PROPERTY, "Theater in der Innenstadt")

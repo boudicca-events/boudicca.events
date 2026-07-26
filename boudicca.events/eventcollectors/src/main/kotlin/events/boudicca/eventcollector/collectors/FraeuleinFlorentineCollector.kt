afc1883f4cb9f03d8722aa552a75d01edfcb9251
abl <ableimat@gmx.at>
2026-07-26 12:11:28 +0200

    fix FraeuleinFlorentineCollector

--- boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/FraeuleinFlorentineCollector.kt
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/FraeuleinFlorentineCollector.kt
@@ -4,110 +4,41 @@ import base.boudicca.SemanticKeys
 import base.boudicca.api.eventcollector.TwoStepEventCollector
 import base.boudicca.api.eventcollector.util.FetcherFactory
 import base.boudicca.api.eventcollector.util.structuredEvent
-import base.boudicca.dateparser.dateparser.DatePair
 import base.boudicca.dateparser.dateparser.DateParser
-import base.boudicca.dateparser.dateparser.DateParserResult
-import base.boudicca.format.UrlUtils
 import base.boudicca.model.structured.StructuredEvent
 import events.boudicca.eventcollector.util.fetchUrlAndParse
 import events.boudicca.eventcollector.util.withDescription
 import org.jsoup.nodes.Element
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
-import java.util.*
 
-class FraeuleinFlorentineCollector : TwoStepEventCollector<Pair<Element, String?>>("fraeuleinflorentine") {
+class FraeuleinFlorentineCollector : TwoStepEventCollector<Element>("fraeuleinflorentine") {
     private val fetcher = FetcherFactory.newFetcher()
     private val baseUrl = "https://frl-florentine.at/eventkalender/"
 
-    override fun getAllUnparsedEvents(): List<Pair<Element, String?>> {
+    override fun getAllUnparsedEvents(): List<Element> {
         val eventSite = fetcher.fetchUrlAndParse(baseUrl)
-        val logoSrc = eventSite.selectFirst(".page-content img")?.attr("src")
 
         return eventSite
-            .select("ul.simcal-events li")
-            .distinctBy { it.text() } // multi-day events have the same text in multiple entries
-            .map { Pair(it, logoSrc) }
+            .select("div.gcal-day-block")
     }
 
-    override fun parseMultipleStructuredEvents(event: Pair<Element, String?>): List<StructuredEvent?> {
-        val (eventSite, logoSrc) = event
-        val nameAndTime = eventSite.select(".simcal-event-title").text().split("|")
+    override fun parseMultipleStructuredEvents(event: Element): List<StructuredEvent?> {
+        val nameAndTime = event.select(".gcal-event-title").text().split("|")
         val name = nameAndTime.first().trim()
-        val description = eventSite.select(".simcal-event-description")
-        var startDate = parseStartDate(eventSite, nameAndTime)
-        val endDate = parseEndDate(eventSite, startDate)
-        if (endDate != null) {
-            startDate = DateParserResult(listOf(DatePair(startDate.dates[0].startDate, endDate.dates[0].startDate)))
-        }
+        val startDate = DateParser.parse(listOf(event.select(".gcal-event-date").text()) + nameAndTime.drop(1))
+
+        val description = event.select(".gcal-event-description")
+        val url = event.select(".gcal-event-link").text()
+        val imageUrl = event.select(".gcal-event-image img").attr("src")
 
         return structuredEvent(name, startDate) {
-            withProperty(SemanticKeys.URL_PROPERTY, UrlUtils.parse(baseUrl))
+            withProperty(SemanticKeys.URL_PROPERTY, url)
             withProperty(SemanticKeys.SOURCES_PROPERTY, listOf(baseUrl))
             withDescription(description)
             withProperty(SemanticKeys.LOCATION_NAME_PROPERTY, "Salonschiff Fräulein Florentine")
-            withProperty(SemanticKeys.LOCATION_URL_PROPERTY, UrlUtils.parse("https://frl-florentine.at"))
+            withProperty(SemanticKeys.LOCATION_URL_PROPERTY, "https://frl-florentine.at")
             withProperty(SemanticKeys.LOCATION_CITY_PROPERTY, "Linz")
-            withProperty(SemanticKeys.PICTURE_URL_PROPERTY, UrlUtils.parse(logoSrc))
+            withProperty(SemanticKeys.PICTURE_URL_PROPERTY, imageUrl)
             withProperty(SemanticKeys.PICTURE_COPYRIGHT_PROPERTY, "Salonschiff Fräulein Florentine")
         }
-    }
-
-    private fun parseStartDate(
-        event: Element,
-        nameAndTime: List<String?>,
-    ): DateParserResult {
-        var startTimeToParse = "00:00"
-        val startTimeElementText = event.select(".simcal-event-start-time").text()
-        if (startTimeElementText.isNotBlank()) {
-            startTimeToParse = startTimeElementText
-            val startTime =
-                LocalTime.parse(
-                    // DateParse can't handle am/pm, so convert it manually and continue with its String
-                    startTimeToParse.uppercase(), // convert pm to PM, to be recognized by the time pattern 'a'
-                    DateTimeFormatter.ofPattern("h:mm a").withLocale(Locale.GERMAN),
-                )
-            startTimeToParse = startTime.toString()
-        } else if (nameAndTime.size > 1 && !nameAndTime[1].isNullOrBlank()) {
-            startTimeToParse = nameAndTime[1]!!.trim()
-        }
-
-        val startDate = DateParser.parse(event.select(".simcal-event-start-date").text(), startTimeToParse)
-
-        return startDate
-    }
-
-    private fun parseEndDate(
-        event: Element,
-        startDate: DateParserResult,
-    ): DateParserResult? {
-        var endTime: LocalTime? = null
-        val endTimeToParse = event.select(".simcal-event-end-time").text()
-        if (endTimeToParse.isNotBlank()) {
-            endTime =
-                LocalTime.parse(
-                    // DateParse can't handle am/pm, so convert it manually and continue with its String
-                    endTimeToParse.uppercase(), // convert pm to PM, to be recognized by the time pattern 'a'
-                    DateTimeFormatter.ofPattern("h:mm a").withLocale(Locale.GERMAN),
-                )
-        }
-
-        var endDate: DateParserResult? = null
-        val endDateToParse = event.select(".simcal-event-end-date").text()
-        if (endDateToParse.isNotBlank()) { // endDate with endDate or end of day if not time is set
-            val time = endTime ?: LocalTime.MAX
-            endDate = DateParser.parse(endDateToParse, time.toString())
-        } else if (endTime != null) { // startDate with endTime
-            endDate =
-                DateParser.parse(
-                    startDate.dates[0]
-                        .startDate
-                        .toLocalDate()
-                        .toString(),
-                    endTime.toString(),
-                )
-        }
-
-        return endDate
     }
 }

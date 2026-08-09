682439add27652805ddb8b5245694524a2ca3f15
abl <ableimat@gmx.at>
2026-08-09 13:38:24 +0200

    fix LastSpaceCollector

--- boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/LastSpaceCollector.kt
+++ boudicca.events/eventcollectors/src/main/kotlin/events/boudicca/eventcollector/collectors/LastSpaceCollector.kt
@@ -5,78 +5,58 @@ import base.boudicca.api.eventcollector.TwoStepEventCollector
 import base.boudicca.api.eventcollector.util.FetcherFactory
 import base.boudicca.api.eventcollector.util.structuredEvent
 import base.boudicca.dateparser.dateparser.DateParser
-import base.boudicca.format.UrlUtils
 import base.boudicca.model.EventCategory
 import base.boudicca.model.structured.StructuredEvent
 import events.boudicca.eventcollector.util.fetchUrlAndParse
 import events.boudicca.eventcollector.util.withDescription
+import java.time.YearMonth
 
 class LastSpaceCollector : TwoStepEventCollector<String>("lastspace") {
     private val fetcher = FetcherFactory.newFetcher()
     private val baseUrl = "https://last-space.at/"
 
-    override fun getAllUnparsedEvents(): List<String> {
-        val pages =
-            fetcher
-                .fetchUrlAndParse(baseUrl + "page/event")
-                .select(".pagination li a")
-                .mapNotNull { it.attr("href") }
-                .filter { it.contains("page") } // to ignore next and previous page buttons
-                .distinct()
-
-        val eventUrls =
-            pages
-                .flatMap { fetchEventUrlsOfSinglePage(it) }
-                .distinct()
-
-        return eventUrls
-    }
-
-    private fun fetchEventUrlsOfSinglePage(page: String): List<String> =
-        fetcher
-            .fetchUrlAndParse(baseUrl + page)
-            .select("tr>td>div>div")
-            .mapNotNull {
-                baseUrl +
-                    it
-                        .attr("onClick")
-                        .replace("'", "")
-                        .replace("window.location.href=/", "")
+    override fun getAllUnparsedEvents(): List<String> =
+        getNext6Months()
+            .map {
+                fetcher.fetchUrlAndParse("${baseUrl}index.php?page=event&filter_year_month=$it")
+            }.flatMap {
+                it.select("div.work-calendar-entry a")
+            }.map {
+                it.attr("abs:href")
             }
 
-    override fun parseMultipleStructuredEvents(event: String): List<StructuredEvent?>? {
+    override fun parseMultipleStructuredEvents(event: String): List<StructuredEvent?> {
         val document = fetcher.fetchUrlAndParse(event)
         val name = document.select("h1").text()
         val description = document.select("div.event-description")
 
-        val eventInfos = document.select("div.event-infos").textNodes()
-        val startDate = DateParser.parse(eventInfos[0].text(), eventInfos[1].text())
+        val startDate = DateParser.parse(document.select("div.event-date").text(), document.select("div.event-time").text())
 
-        val imgSrc = document.select(".slide-img img").attr("src")
-        val tags =
-            eventInfos[4]
-                .text()
-                .split(",")
-                .map { it.replace("#", "").trim() }
-                .filter { it.isNotBlank() }
-
-        val type = eventInfos[3].text().trim()
-        var category = EventCategory.OTHER
-        if (type.lowercase().contains("sport")) {
-            category = EventCategory.SPORT
-        }
+        val imgSrc = document.select(".slide-img img").attr("abs:src")
+        val tags = document.select("div.event-detail-category-tag").map { it.text().removePrefix("#") }
+        val type = tags[0]
+        val category =
+            if (type.lowercase().contains("sport")) {
+                EventCategory.SPORT
+            } else {
+                EventCategory.OTHER
+            }
 
         return structuredEvent(name, startDate) {
-            withProperty(SemanticKeys.URL_PROPERTY, UrlUtils.parse(event))
-            withProperty(SemanticKeys.SOURCES_PROPERTY, listOf(baseUrl))
+            withProperty(SemanticKeys.URL_PROPERTY, event)
+            withProperty(SemanticKeys.SOURCES_PROPERTY, baseUrl)
             withDescription(description)
             withProperty(SemanticKeys.CATEGORY_PROPERTY, category)
             withProperty(SemanticKeys.TYPE_PROPERTY, type)
             withProperty(SemanticKeys.TAGS_PROPERTY, tags)
-            withProperty(SemanticKeys.PICTURE_URL_PROPERTY, UrlUtils.parse(baseUrl, imgSrc))
+            withProperty(SemanticKeys.PICTURE_URL_PROPERTY, imgSrc)
             withProperty(SemanticKeys.PICTURE_COPYRIGHT_PROPERTY, "last")
             withProperty(SemanticKeys.LOCATION_CITY_PROPERTY, "Linz")
             withProperty(SemanticKeys.LOCATION_NAME_PROPERTY, "last")
         }
     }
+
+    private fun getNext6Months(): List<YearMonth> =
+        (0L until 6L)
+            .map { YearMonth.now().plusMonths(it) }
 }
